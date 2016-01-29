f425e530be0c137bfb657730170603af099610aa
git-user <git-email>
2016-01-29 18:23:44 +0000

    dependency issues with pkconfig

--- pykern/pkcli/__init__.py
+++ pykern/pkcli/__init__.py
@@ -54,7 +54,6 @@ Example:
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from __future__ import absolute_import, division, print_function
-from pykern.pkdebug import pkdc, pkdp
 
 import argparse
 import importlib
