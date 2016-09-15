7558a2e7bb40ca4fbccb50f997c684373d2ef495
robnagler <github@q33.us>
2016-09-15 03:05:34 +0000

    AssertionError.msg works on 2.7.10, but not 2.7.9

--- tests/pkunit_test.py
+++ tests/pkunit_test.py
@@ -80,8 +80,8 @@ def test_pkexcept():
         with pkexcept(KeyError, 'xyzzy'):
             pass
     except AssertionError as e:
-        assert 'xyzzy' in e.msg
-        assert 'pkunit_test.py:{}:test_pkexcept'.format(lineno) in e.msg
+        assert 'xyzzy' in e.message
+        assert 'pkunit_test.py:{}:test_pkexcept'.format(lineno) in e.message
     except Exception as e:
         pkfail('{}: got exception, but not AssertionError', e)
     else:
@@ -90,7 +90,7 @@ def test_pkexcept():
         with pkexcept(KeyError):
             raise NameError('whatever')
     except AssertionError as e:
-        assert re.search(r'exception was raised.*but expected.*KeyError', e.msg)
+        assert re.search(r'exception was raised.*but expected.*KeyError', e.message)
     except Exception as e:
         pkfail('{}: got exception, but not AssertionError', e)
     else:
@@ -100,7 +100,7 @@ def test_pkexcept():
         with pkexcept('any pattern'):
             pass
     except AssertionError as e:
-        assert 'pkunit_test.py:{}:test_pkexcept'.format(lineno) in e.msg
+        assert 'pkunit_test.py:{}:test_pkexcept'.format(lineno) in e.message
     except Exception as e:
         pkfail('{}: got exception, but not AssertionError', e)
     else:
@@ -117,7 +117,8 @@ def test_pkok():
     try:
         pkok(0, 'xyzzy {} {k1}', '333', k1='abc')
     except AssertionError as e:
-        assert 'pkunit_test.py:{}:test_pkok xyzzy 333 abc'.format(lineno) == e.msg
+        # May not match exactly, because depends on start directory
+        assert 'pkunit_test.py:{}:test_pkok xyzzy 333 abc'.format(lineno) in e.message
 
 
 def _expect(base):
