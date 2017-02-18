60448a3dbe0be4787b35196655d13cd9bcce0d9d
robnagler <github@q33.us>
2017-02-18 01:12:43 +0000

    ckp

---
+++ pykern/pkcli/rsmanifest.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+u"""Create and read global and user manifests.
+
+:copyright: Copyright (c) 2017 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+
+# Appears in each directory
+BASENAME = 'rsmanifest.json'
+
+#POSIT: https://github.com/radiasoft/containers/blob/master/bin/build build_rsmanifest()
+# Written once at build time
+CONTAINER_FILE = '/' + BASENAME
+
+# Read and written multiple times as the run user
+USER_FILE = '~/' + BASENAME
+
+# Identifies codes which are not installed in a virtualenv
+_NO_VENV = ''
+
+
+def add_code(name, version, uri, source_d, virtual_env=None):
+    """Add a new code to ~?rsmanifest.json
+
+    Args:
+        name (str): name of the package
+        version (str): commit or version
+        uri (str): repo, source link
+        source_d (str): directory containing
+        virtual_env (str): name of the virtual_env to qualify
+    """
+    from pykern import pkcollections
+    from pykern import pkio
+    import json
+    import os.path
+
+    fn = os.path.expanduser(USER_FILE)
+    try:
+        with open(fn) as f:
+            values = pkcollections.json_load_any(f)
+    except Exception as e:
+        if not (pkio.exception_is_not_found(e) or isinstance(e, ValueError)):
+            raise
+        values = pkcollections.Dict(
+            version='20170217.180000',
+            codes=pkcollections.Dict({_NO_VENV: pkcollections.Dict()}),
+        )
+    if not virtual_env:
+        virtual_env = _NO_VENV
+    v = values.codes.get(virtual_env) or pkcollections.Dict()
+    v[name.lower()] = pkcollections.Dict(
+        name=name,
+        source_d=source_d,
+        uri=uri,
+        version=version,
+    )
+    values.codes[virtual_env] = v
+    with open(fn, 'w') as f:
+        f.write(
+            json.dumps(values, indent=4, separators=(',', ': '), sort_keys=True) + "\n"
+        )
