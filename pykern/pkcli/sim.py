715e972e0487d1677f8eaec97bd18460d4c8702f
robnagler <github@q33.us>
2017-03-25 01:03:26 +0000

    added _init() which pushes to bitbucket
    github doesn't allow private commits; gist.github doesn't allow subdirs

--- pykern/pkcli/sim.py
+++ pykern/pkcli/sim.py
@@ -6,8 +6,11 @@ u"""wrapper for running simulations
 """
 from __future__ import absolute_import, division, print_function
 
+
 def default_command(cmd, *args, **kwargs):
+    """Wrapper until figure out *args with argh"""
     import sys
+
     return getattr(sys.modules[__name__], '_'+ cmd)(*args, **kwargs)
 
 
@@ -21,8 +24,49 @@ def _run(*args):
     import py.path
     import os
 
-    venv = py.path.local('venv')
+    #TODO(robnagler) need to .gitignore output files
+    venv = py.path.local('rs-venv')
     env = os.environ.copy()
     env['PATH'] = str(venv.join('bin')) + ':' + env['PATH']
     env['PYTHONUSERBASE'] = str(venv)
     subprocess.check_call(args, env=env)
+
+
+def _init():
+    import requests
+    import subprocess
+    import os
+    import py.path
+    import datetime
+    from pykern import pkcli
+
+    #TODO(robnagler) configure bitbucket locally for each repo
+    try:
+        gid = subprocess.check_output(['git', 'config', 'user.name']).strip()
+    except subprocess.CalledProcessError:
+        pckli.command_error('please "git login" first')
+    title = py.path.local().basename
+    v = datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S')
+    name='sim-{}-{}'.format(py.path.local().basename, v).lower()
+    url = 'https://api.bitbucket.org/2.0/repositories/{}/{}'.format(gid, name)
+    r = requests.post(
+        url,
+        json=dict(
+            scm='git',
+            is_private=True,
+            fork_policy='no_public_forks',
+            name=name,
+        ),
+    )
+    if r.status_code != 200:
+        pkcli.command_error('{}: post failed: {} {}', url, r, r.text)
+    repo_url = r.json()['links']['clone'][0]['href']
+    #TODO(robnagler) add README.md if not already there
+    subprocess.check_call(['git', 'init'])
+    subprocess.check_call(['git', 'remote', 'add', 'origin', repo_url])
+    subprocess.check_call(['git', 'checkout', '-b', 'master'])
+
+    #TODO(robnagler) do every run(?)
+    subprocess.check_call(['git', 'add', '.'])
+    subprocess.check_call(['git', 'commit', '-m', 'init'])
+    subprocess.check_call(['git', 'push', '-u', 'origin', 'master'])
