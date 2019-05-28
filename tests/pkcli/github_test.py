4bf1a14f6b6d3b30f30732bb45f4e8a501dfcbf6
robnagler <github@q33.us>
2019-05-28 17:00:46 +0000

    test github backup

---
+++ tests/pkcli/github_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+u"""test github
+
+:copyright: Copyright (c) 2019 Bivio Software, Inc.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+import pytest
+
+def test_backup():
+    from pykern import pkconfig
+
+    pkconfig.reset_state_for_testing({
+        'PYKERN_PKCLI_GITHUB_TEST_MODE': '1',
+        'PYKERN_PKCLI_GITHUB_API_PAUSE_SECONDS': '0',
+    })
+    from pykern.pkcli import github
+    from pykern import pkunit
+    from pykern import pkio
+
+    with pkunit.save_chdir_work():
+        github.backup()
+        github.backup()
