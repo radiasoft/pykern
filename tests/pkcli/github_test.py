2f095339fa2b9bf7401e60cde7fdb3e1debd1de7
robnagler <github@q33.us>
2019-05-29 15:56:51 +0000

    fix #40 skip test if travis

--- tests/pkcli/github_test.py
+++ tests/pkcli/github_test.py
@@ -6,7 +6,9 @@ u"""test github
 """
 from __future__ import absolute_import, division, print_function
 import pytest
+import os
 
+@pytest.mark.skipif(os.environ.get('TRAVIS'), reason='travis uses shared IPs so gets rate limited too easily')
 def test_backup():
     from pykern import pkconfig
 
