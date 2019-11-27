2eb3dbd5086ed7a22ab580addb98272109512266
robnagler <github@q33.us>
2019-11-27 00:05:27 +0000

    added case=; error on no tests; added -s -v
    case=foo translates to pytest -k foo
    Added -s and -v so output in log is always useful

--- pykern/pkcli/test.py
+++ pykern/pkcli/test.py
@@ -8,16 +8,22 @@ from __future__ import absolute_import, division, print_function
 from pykern.pkcollections import PKDict
 
 
-def default_command(*tests):
+def default_command(*args):
     """Run tests one at a time with py.test.
 
-    Searches in ``tests`` sub-directory if not provide a
+    Searches in ``tests`` sub-directory if not provided a
     list of tests.
+
+    Arguments are directories or files, which are searched for _test.py
+    files.
+
+    An argument which is ``case=<pattern>``, is passed to pytest
+    as ``-k <pattern>``.
 
     Writes failures to ``<base>_test.log``
 
     Args:
-        tests (str): name of tests [all files ``tests``]
+        args (str): test dirs, files, options
     Returns:
         str: passed=N if all passed, else raises `argh.CommandError`
     """
@@ -31,7 +37,9 @@ def default_command(*tests):
     e = PKDict(os.environ)
     n = 0
     f = []
-    for t in _find(tests):
+    c = []
+    paths, flags = _args(args)
+    for t in paths:
         n += 1
         o = t.replace('.py', '.log')
         m = 'pass'
@@ -39,11 +47,13 @@ def default_command(*tests):
             sys.stdout.write(t)
             sys.stdout.flush()
             pksubprocess.check_call_with_signals(
-                ('py.test', '--tb=native', t),
+                ['py.test', '--tb=native', '-v', '-s', t] + flags,
                 output=o,
                 env=PKDict(
                     os.environ,
-                ).pkupdate({pkunit.TEST_FILE_ENV: t})
+                ).pkupdate({pkunit.TEST_FILE_ENV: t}),
+#TODO(robnagler) not necessary
+#                recursive_kill=True,
             )
         except Exception as e:
             if isinstance(e, RuntimeError) and 'exit(5)' in e.args[0]:
@@ -54,23 +64,40 @@ def default_command(*tests):
                 m = 'FAIL {}'.format(o)
                 f.append(o)
         sys.stdout.write(' ' + m + '\n')
+    if n == 0:
+        pkcli.command_error('no tests found')
     if len(f) > 0:
         # Avoid dumping too many test logs
         for o in f[:5]:
             sys.stdout.write(pkio.read_text(o))
         sys.stdout.flush()
-        pkcli.command_error('FAILED={} passed={}\n'.format(len(f), n - len(f)))
+        pkcli.command_error('FAILED={} passed={}'.format(len(f), n - len(f)))
     return 'passed={}'.format(n)
 
 
-def _find(tests):
+def _args(tests):
+    paths = []
+    flags = []
+    for t in tests:
+        if '=' in t:
+            a, b = t.split('=')
+            if a == 'case':
+                flags.extend(('-k', b))
+            else:
+                pkcli.command_error('unsupported option={}'.format(t))
+        else:
+            paths.append(t)
+    return _find(paths), flags
+
+
+def _find(paths):
     from pykern import pkio
     import re
 
     i = re.compile(r'(?:_work|_data)/')
     res = []
     cwd = pkio.py_path()
-    for t in tests or ('tests',):
+    for t in paths or ('tests',):
         t = pkio.py_path(t)
         if t.check(file=True):
             res.append(str(cwd.bestrelpath(t)))
