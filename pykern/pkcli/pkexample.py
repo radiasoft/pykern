24f689fa368bf61f3fc76dcd32b8b6daace96d53
robnagler <github@q33.us>
2021-09-23 16:07:37 -0600

    remove cruft

--- pykern/pkcli/pkexample.py
+++ pykern/pkcli/pkexample.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-u"""Demonstrate how to write reusable command line tools.
+"""Demonstrate how to write reusable command line tools.
 
 A `pykern.pkcli` module is designed to be used from any context
 any other piece of code. Since we use `argh`, it makes it easy
@@ -59,7 +59,6 @@ any arguments::
 :copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
 from pykern import pkcli
 import itertools
 
@@ -84,47 +83,3 @@ def echo(suffix, prefix='howdy: '):
     # return the value. This way the function can be used in
     # other contexts. argh prints the return of the function to stdout.
     return prefix + suffix
-
-
-def primes(max_prime, timeit=0):
-    """Compute primes less than `max_prime`
-
-    Args:
-        max_prime (int): maximum number of primes
-        timeit (int): run timeit function with this many loops
-
-    Returns:
-        object: list of primes (list) or timeit result (str)
-    """
-    max_prime = int(max_prime)
-    if max_prime <= 0:
-        pkcli.command_error('{}: max_prime must be positive', max_prime)
-    timeit = int(timeit)
-    if timeit < 0:
-        pkcli.command_error('{}: timeit must be non-negative', timeit)
-    primes = _sundaram3(max_prime)
-    if timeit == 0:
-        return primes
-    import timeit as t
-    secs = t.timeit(
-        stmt='_sundaram3({})'.format(max_prime),
-        number=timeit,
-        setup='from pykern.pkcli.pkexample import _sundaram3',
-    )
-    return 'computed {:,} primes in {:.3f} seconds'.format(len(primes), secs)
-
-
-def _sundaram3(max_n):
-    """Returns list of primes less than `max_n`.
-
-    Copied from `stackoverflow <http://stackoverflow.com/a/2073279>`_.
-    """
-    numbers = list(range(3, max_n + 1, 2))
-    half = max_n // 2
-    initial = 4
-    for step in xrange(3, max_n + 1, 2):
-        for i in xrange(initial, half, step):
-            numbers[i - 1] = 0
-        initial += 2 * (step + 1)
-        if initial > half:
-            return [2] + filter(None, numbers)
