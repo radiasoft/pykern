5a97f7468a3130ed65d048baf9579043b40e3850
gurhar1133 <ghns1133@gmail.com>
2022-03-14 21:32:21 +0000

    Fix #135: constants for rslaser and rsbeam

--- pykern/pkconst.py
+++ pykern/pkconst.py
@@ -5,6 +5,8 @@ u"""Various constant values
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from __future__ import absolute_import, division, print_function
+import math
+import scipy.constants
 
 # pykern uses pksetup in setup.py so requirements.txt is not yet evaluated so can't use six
 # or any other external dependencies. The "types" modules in Python 2 had a StringTypes, which
@@ -31,3 +33,33 @@ except Exception:
 
 #: Copied from numconv, which copied from RFC1924
 BASE62_CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+TWO_PI = 2 * math.pi
+RT_TWO_PI = math.sqrt(2*math.pi)
+RT_2_OVER_PI = math.sqrt(2/math.pi)
+pi = math.pi
+RT_2 = math.sqrt(2.)
+TWO_PI = 2 * math.pi
+RT_TWO_PI = math.sqrt(2*math.pi)
+RT_2_OVER_PI = math.sqrt(2/math.pi)
+c = scipy.constants.c                  # speed of light [m/s]
+c_SQ = scipy.constants.c**2
+c_INV  = 1./scipy.constants.c
+mu_0 = scipy.constants.mu_0            # permeability of free space
+epsilon_0 = scipy.constants.epsilon_0  # permittivity of free space
+m_p = scipy.constants.m_p              # proton mass [kg]
+m_e = scipy.constants.m_e              # electron mass [kg]
+e = scipy.constants.e                  # fundamental electric charge [C] (positive)
+MKS_factor = 1./(4.*math.pi*scipy.constants.epsilon_0)
+KG_to_EV = c_SQ / e          # convert mass [kg] to effective energy [eV]
+EV_to_ERG = 1.602e-12        # convert energy from eV to erg
+C_to_STATC = scipy.constants.c * 10.   # convert charge from coulombs to statcoulombs
+e_CGS = scipy.constants.e * C_to_STATC
+c_CGS = scipy.constants.c * 100
+m_e_CGS = scipy.constants.m_e * 1000.
+m_p_CGS = scipy.constants.m_p * 1000.
+m_e_EV = scipy.constants.m_e * KG_to_EV
+m_p_EV = scipy.constants.m_p * KG_to_EV
+C_SQ = scipy.constants.c**2
+C_INV  = 1./scipy.constants.c
+MKS_FACTOR = 1./(4.*math.pi*scipy.constants.epsilon_0)
+M_E_EV = scipy.constants.m_e * C_SQ / (-scipy.constants.e)
