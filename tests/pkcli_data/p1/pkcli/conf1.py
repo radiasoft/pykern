a0bf5da6229241c6a00533e1f523d15fc3f24980
Gurhar Khalsa <49498231+gurhar1133@users.noreply.github.com>
2022-04-18 15:59:51 -0600

    Fix #156: better help message (#160)

--- tests/pkcli_data/p1/pkcli/conf1.py
+++ tests/pkcli_data/p1/pkcli/conf1.py
@@ -5,11 +5,21 @@ last_cmd = None
 from pykern.pkdebug import pkdp
 
 def cmd1(arg1):
+    """Subject line for cmd1
+
+    Args:
+        arg1
+    """
     global last_cmd
     last_cmd = cmd1
     return
 
 def cmd2():
+    """Subject line for cmd2
+
+    Args:
+        -
+    """
     global last_cmd
     last_cmd = cmd2
     return
