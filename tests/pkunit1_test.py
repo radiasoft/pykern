c22bdbc223e226b1401e71284df548c5ab56ba0f
gurhar1133 <ghns1133@gmail.com>
2022-05-02 22:59:19 +0000

    Fix #155: test WIP

--- tests/pkunit1_test.py
+++ tests/pkunit1_test.py
@@ -8,8 +8,23 @@ import pytest
 
 def test_case_dirs():
     from pykern import pkunit
+    from pykern.pkdebug import pkdp
+
+    pkdp('case_dirs: {}', pkunit.case_dirs())
+    for d in pkunit.case_dirs():
+
+        pkdp('D: {}', d)
+        pkdp('D base: {}', d.basename)
+        if d.basename != 'xlsx':
+            i = d.join('in.txt').read()
+            pkdp('I: {}', i)
+            pkdp('d.basename: {}', d.basename)
+            pkunit.pkeq(d.basename + '\n', i)
+            d.join('out.txt').write(i)
+
+def test_xlsx_to_csv_conversion():
+    from pykern import pkunit
+    from pykern.pkdebug import pkdp
 
     for d in pkunit.case_dirs():
-        i = d.join('in.txt').read()
-        pkunit.pkeq(d.basename + '\n', i)
-        d.join('out.txt').write(i)
+        pkdp('xlsx ? D: {}', d)
