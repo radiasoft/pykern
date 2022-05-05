3c9f125c58efc4b7b9ff72e4283b3a4859d926a9
robnagler <github@q33.us>
2022-05-05 00:05:12 +0000

    fix #165 setuptools 62.1.0 works

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
         'pytz>=2015.4',
         'ruamel.yaml>=0.16.0',
         'requests>=2.18',
-        # POSIT: pksetup.setup asserts setuptools.__version__ < 57
-        'setuptools<57',
+        # setuptools breaks almost every release so limiting is safer than not
+        'setuptools<63',
         'six>=1.9',
         'Sphinx>=1.3.5',
         'twine>=1.9',
