e67d0a395a8e29aa0d00e1ec8b262ae6d9041f7b
gurhar1133 <ghns1133@gmail.com>
2022-06-06 18:54:19 +0000

    Fix #191: test edit

---
+++ tests/pkcli/fmt_data/file1_expect.py
@@ -0,0 +1,22 @@
+def sqaunch(
+    this,
+    be,
+    a,
+    very,
+    poorly,
+    formatted,
+    function,
+    that,
+    noone,
+    should,
+    ever,
+    use,
+    it,
+    has,
+    way,
+    too,
+    many,
+    damn,
+    arguments,
+):
+    print("squanch")
---
+++ tests/pkcli/fmt_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+u"""test fmt
+
+:copyright: Copyright (c) 2019 Bivio Software, Inc.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdp
+import pytest
+
+def test_edit():
+    from pykern import pkunit
+    from pykern import pkio
+    from pykern.pkcli import fmt
+
+    pkunit.data_dir().join('file1.py').copy(pkunit.empty_work_dir().join('file1.py'))
+    actual_path = pkunit.work_dir().join('file1.py')
+    fmt.edit(actual_path)
+    pkunit.file_eq(
+        expect_path=pkunit.data_dir().join('file1_expect.py'),
+        actual_path=actual_path
+    )
+
+def test_diff():
+    pass
