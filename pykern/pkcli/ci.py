3edf765215be344bd0ce618b50cf6f18fdc5a5b3
gurhar1133 <ghns1133@gmail.com>
2022-06-16 20:25:43 +0000

    Fix #202: with reg subprocess

--- pykern/pkcli/ci.py
+++ pykern/pkcli/ci.py
@@ -5,14 +5,31 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from pykern.pkdebug import pkdp, pkdlog
+import pykern.pksubprocess
+import subprocess
+
+
+def check_prints():
+    """Recursively check repo for pkdp() calls"""
+    from pykern import pksubprocess
+    from pykern import pkio
+    from pykern import pkcompat
+
+    proc = subprocess.Popen([
+            "grep -r -l 'pkdp(' | grep '.py$'",
+        ],
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = proc.communicate()
+    print("OUTPUT:\n", pkcompat.from_bytes(stdout), "ERR: ", stderr)
+
 
 def run():
     """Run the continuous integration checks and tests
         * Checks formatting
         * Runs test suite
-
-    Args:
-        dir (object): string or py.path to file or directory
     """
     from pykern.pkcli import fmt, test
     from pykern import pkio
