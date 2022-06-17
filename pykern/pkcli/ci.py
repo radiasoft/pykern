bd387b3cdb5ef7f744a8628edc4e0f675d04d972
gurhar1133 <ghns1133@gmail.com>
2022-06-17 21:34:34 +0000

    Fix #202: consts for re

--- pykern/pkcli/ci.py
+++ pykern/pkcli/ci.py
@@ -5,28 +5,30 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from pykern.pkdebug import pkdp, pkdlog
+from pykern import pkunit
+from pykern.pkcli import test
+from pykern import pksetup
 import re
 
 
 _FILE_TYPE = re.compile(r'.py$')
+_EXCLUDE_FILES = re.compile( f"/{test.SUITE_D}/.*{pkunit.DATA_DIR_SUFFIX}/|/{test.SUITE_D}/.*_work/|/{pksetup.PACKAGE_DATA}/|pkdebug")
+_PRINT = re.compile('\s(pkdp)\(|\s(print)\(')
+
 
 def check_prints():
     """Recursively check repo for pring and pkdp calls"""
     from pykern import pkconst
     from pykern import pkio
-    from pykern import pkunit
-    from pykern.pkcli import test
-    from pykern import pksetup
 
     fails = False
-    e = f"/{test.SUITE_D}/.*{pkunit.DATA_DIR_SUFFIX}/|/{test.SUITE_D}/.*_work/|/{pksetup.PACKAGE_DATA}/|pkdebug"
     for f in pkio.walk_tree(pkio.py_path(), _FILE_TYPE):
-        if _match(re.compile(e), str(f)):
+        if _match(re.compile(_EXCLUDE_FILES), str(f)):
             continue
 
         s = pkio.read_text(str(f))
         for i, l in enumerate(s.split('\n')):
-            if _match('\s(pkdp)\(|\s(print)\(', l):
+            if _match(_PRINT, l):
                 fails = True
                 pkconst.builtin_print(f'{f} pkdp/print on line: {i + 1} :-> {l}')
     return fails
