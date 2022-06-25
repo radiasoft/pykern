2b472af6f43b56d0350340a2e2aa778c176f3a7f
robnagler <github@q33.us>
2022-06-25 17:04:31 +0000

    added test for canonicalize

--- tests/pkcollections_test.py
+++ tests/pkcollections_test.py
@@ -8,6 +8,30 @@ from __future__ import absolute_import, division, print_function
 import pytest
 
 
+def test_canonicalize():
+    from pykern.pkunit import pkeq, pkexcept
+    from pykern.pkcollections import PKDict, canonicalize
+    import decimal
+
+    class _i(int):
+        pass
+
+    class _o:
+        pass
+
+    pkeq(None, canonicalize(None))
+    pkeq(3.3, canonicalize(decimal.Decimal(3.3)))
+    pkeq(int, type(canonicalize(_i(1))))
+    pkeq([1], canonicalize(set([1])))
+    pkeq(['bytes', 'str'], canonicalize(iter([b'bytes', 'str'])))
+    pkeq([9.01, True, False], canonicalize((9.01, True, False)))
+    d = canonicalize({_i(-1): dict(a='b', c=frozenset([13]))})
+    pkeq(PKDict({-1: PKDict(a='b', c=[13])}), d)
+    pkeq(int, type(list(d.keys())[0]))
+    with pkexcept('unable to canonicalize'):
+        canonicalize(_o())
+
+
 def test_delitem():
     from pykern.pkcollections import PKDict
 
