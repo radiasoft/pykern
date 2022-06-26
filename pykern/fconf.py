7817f1a544256835b1a2cbe4150730716ab2d384
robnagler <github@q33.us>
2022-06-26 11:48:40 -0600

    local_fvars not getting expanded

--- pykern/fconf.py
+++ pykern/fconf.py
@@ -1,58 +1,27 @@
 # -*- coding: utf-8 -*-
-u"""File-based configuration
+"""File-based configuration
 
-fc_arg/child/content is the content argument
-fc_raw_child is the raw data (unevaled)
-fc_<func> are builtins
-depth first eval
-templates eval values
-templates: is the section
-functions: are in python
-macros: yield
-   they can have variables ${arg}
-   in the
-
-template args substituted before eval
-
-for macros that are loops (multi yield)
-    the return is always an array or dict
-    depending on what context it is executing in
-
-self is dynamic
-
-return value can be dynamic created off self, e.g.
-     return seflf.fc_replace(self.fc_parent(), etc.)
-
-# if *args, defer eval args
-# if a function begins with_ then must yield and eval is deferred
-
-
-TODO: hold state on "self". Maybe a special function fconf_var().get/put
-
-  _mpi_worker_prod(): |
-    mpi_worker:
-      clusters:
-      {% for k, v in _host_mpi_user.items() %}
-        "{{ k }}":
-          {% for i in v %}
-          - {{ _fnl(i) }}
-          {% do _host_mpi.append(i) %}
-          {% endfor %}
-      {% endfor %}
-    {% endif %}
-
+fconf_clear()
+fconf_inner() -- already evaluated? yes by default
+${} for global_fvars() on namespace? how would it work?
+convert rsconf
+class for fconf macros, can that just be _Namespace?
+escape \\${}
 
 :copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdlog, pkdp
+import contextlib
 import copy
 import inspect
 import pykern.pkrunpy
 import re
 import pykern.pkyaml
 import pykern.pkcollections
+import pykern.pkio
+
 
 #: parse_files macro expansion pattern
 _TEMPLATE_NAME_RE = re.compile(r'^([a-z]\w*)\(((?:\s*[a-z]\w*\s*)?(?:,\s*[a-z]\w*\s*)*)\)$', flags=re.IGNORECASE)
@@ -61,12 +30,13 @@ _MACRO_CALL_RE = re.compile(r'^([a-z]\w*)\((.*)\)$', flags=re.IGNORECASE+re.DOTA
 
 _ARGS_RE = re.compile(r'\s*,\s*|\s+')
 
-_TEMPLATE_CALL_SUB = re.compile(r'\$\{(\w+)\}')
-_TEMPLATE_CALL_EXACT = re.compile('^' + _TEMPLATE_CALL_SUB.pattern + '$')
+_FVAR_RE = re.compile(r'\$\{(\w+)\}')
+_FVAR_EXACT_RE = re.compile('^' + _FVAR_RE.pattern + '$')
 
 _SELF = '_self'
 
 _NO_PARAM = object()
+
 
 class Parser(PKDict):
 
@@ -87,11 +57,10 @@ class Parser(PKDict):
             except Exception:
                 pkdlog('error parsing file={}', f)
                 raise
-        res = PKDict()
         e = _Evaluator(parser=self)
         for f in self.files.yml:
-            res = e.do(f, res)
-        self.result = res
+            e.do(source=f)
+        self.result = e.global_fvars
 
     def _add_file(self, path):
         e = path.ext[1:].lower()
@@ -159,22 +128,179 @@ class Parser(PKDict):
         return pykern.pkyaml.load_file(path)
 
 
+
+
+class _Evaluator(PKDict):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # By not passing __builtins__=PKDict(), builtins are implicitly added.
+        # Builtins are useful
+        self.global_ns = PKDict()
+        self.local_ns = _Namespace(self)
+        self.global_fvars = PKDict()
+        self.local_fvars = PKDict()
+
+    def do(self, source, **kwargs):
+        p = self.local_fvars
+        i = 'local_fvars' in kwargs
+        if i:
+            self.local_fvars = kwargs['local_fvars']
+            base = None
+        else:
+            if not (source.content is None or isinstance(source.content, PKDict)):
+                raise ValueError(f'{source} must be PKDict or empty')
+            base = self.global_fvars
+            self.xpath = _XPath()
+        try:
+            with self._xpath(kwargs.get('xpath_element')):
+                return self._do(source.content, base)
+        except Exception:
+            if not i:
+                pkdlog('Error evaluating {} {}', source, self.xpath)
+            raise
+        finally:
+            self.local_fvars = p
+
+    def _dict(self, new, base):
+        if not isinstance(base, PKDict):
+            if base is not None:
+                raise ValueError(f'mismatched types new={new} base={base}')
+            base = PKDict()
+        for k, v in new.items():
+#TODO: pass v to expr
+            with self._xpath(k):
+                x = self._expr(k)
+                if isinstance(x, PKDict):
+                    #TODO better error msgs
+                    assert v is None, f'key={k} must not have a value'
+                    base.pkmerge(x)
+                elif isinstance(x, list):
+                    raise ValueError(
+                        f'mismatched types expanding macro={k} to value={x}'
+                        + ' into dict={res}',
+                        )
+                else:
+                    assert x is not None, f'expanded macro={k} to None'
+                    with self._xpath(x):
+                        base[x] = self._do(v, base.get(x))
+        return base
+
+    def _do(self, new, base):
+        if isinstance(new, PKDict):
+            return self._dict(new, base)
+        if isinstance(new, list):
+            return self._list(new, base)
+        return self._expr(new)
+
+    def _expr(self, value):
+
+        def _fvar(match):
+            n = match.group(1)
+            if n not in self.local_fvars:
+                TypeError(f'unknown template fvar={n}')
+            return self.local_fvars[n]
+
+        with self._xpath(value):
+            if not isinstance(value, str):
+                # already canonicalized
+                return value
+            m = _FVAR_EXACT_RE.search(value)
+            # This check prevents stringification of data types on exact
+            # matches, which is important for non-string fvars
+            if m:
+                v = _fvar(m)
+                if not isinstance(v, str):
+                    # already canonicalized
+                    return v
+            else:
+                v = _FVAR_RE.sub(_fvar, value)
+            m = _MACRO_CALL_RE.search(v)
+            if m:
+                return eval(
+                    f'{m.group(1)}({_SELF},{m.group(2)})',
+                    self.global_ns,
+                    self.local_ns,
+                )
+        return value
+
+    def _list(self, new, base):
+        if not isinstance(base, list):
+            if base is not None:
+                raise ValueError(f'mismatched types new={new} base={base}')
+            base = []
+        res = []
+        for i, e in enumerate(new):
+            # lists don't have base values
+            with self._xpath(f'[{i}]'):
+                x = self._do(e, None)
+            if isinstance(x, list):
+                res.extend(x)
+            else:
+                res.append(x)
+        return res + base
+
+    @contextlib.contextmanager
+    def _xpath(self, element=None):
+        """Track element path for errors
+
+        If there is an exception, xpath will hold the place of the exception
+        """
+        if element is not None:
+            self.xpath.push(element)
+            pkdp('> {}', self.xpath.top())
+            yield
+            self.xpath.pop()
+            pkdp('<')
+        else:
+            yield
+
+
 class _File(PKDict):
 
     def __str__(self):
-        return 'source=' + pkio.py_path().bestrelpath(self.path)
+        return 'source=' + pykern.pkio.py_path().bestrelpath(self.path)
 
 
 class _Macro(PKDict):
 
     def call(self, namespace, args, kwargs):
-        return self.func(namespace, *args, **kwargs)
+        return pykern.pkcollections.canonicalize(
+            self.func(namespace, *args, **kwargs),
+        )
 
     def _kind(self):
         return 'macro'
 
     def __str__(self):
         return f'{self._kind}={self.name} {self.source}'
+
+
+class _Namespace():
+
+    def __init__(self, evaluator):
+        self._evaluator = evaluator
+
+    def __func(self, name, exc):
+        m = self._evaluator.parser.macros.get(name)
+        if not m:
+            raise exc(f'macro name={name}')
+
+        def x(*args, **kwargs):
+            a = list(args)
+            if a and a[0] is self:
+                a.pop(0)
+            return m.call(self, a, kwargs)
+
+        return x
+
+    def __getattr__(self, name):
+        return self.__func(name, AttributeError)
+
+    def __getitem__(self, name):
+        if name == _SELF:
+            return self
+        return self.__func(name, KeyError)
 
 
 class _Template(PKDict):
@@ -189,34 +315,11 @@ class _Template(PKDict):
             s.add(p)
 
     def call(self, namespace, args, kwargs):
-        kwargs = self._parse_args(args, kwargs)
-
-        def _expr(evaluator, value):
-            m = _TEMPLATE_CALL_EXACT.search(value)
-            if m:
-                # Prevents stringification of data types
-                return True, _repl(m)
-            return True, _TEMPLATE_CALL_SUB.sub(_repl, value)
-
-        def _repl(match):
-            n = match.group(1)
-            if n not in kwargs:
-                TypeError(f'unknown arg={n} referenced in macro={self.name}')
-            return kwargs[n]
-
         return namespace._evaluator.do(
-            # Templates are expanded then checked for macros
-            _TemplateExpanded(
-                content= _Evaluator(
-                    parent=namespace._evaluator,
-                    expr_op=_expr,
-                ).do(self, None),
-            ),
-            #TODO(robnagler) may not be right. A template can expand to match
-            # the existing base it is being plugged into. The merge would happen
-            # in _dict() below after the expansion. It's a bit of a different model,
-            # but maybe not, because we are trying to avoid double evals.
-            None,
+            base=None,
+            local_fvars=self._parse_args(args, kwargs),
+            source=self,
+            xpath_element=self.name + '()',
         )
 
     def _kind(self):
@@ -246,131 +349,32 @@ class _Template(PKDict):
     def __str__(self):
         return f'template={self.name}'
 
-class _TemplateExpanded(PKDict):
 
-    def __str__(self):
-        return f'expanded {self.template}'
+class _XPath():
 
-class _Namespace():
+    def __init__(self):
+        self.stack = []
 
-    def __init__(self, evaluator):
-        self._evaluator = evaluator
+    def pkdebug_str(self):
+        res = 'XPath:\n'
+        for i in self.stack:
+            res += self._str(i)
+        return res
 
-    def __func(self, name, exc):
-        m = self._evaluator.parser.macros.get(name)
-        if not m:
-            raise exc(f'macro name={name}')
-
-        def x(*args, **kwargs):
-            a = list(args)
-            if a and a[0] is self:
-                a.pop(0)
-            return m.call(self, a, kwargs)
-
-        return x
-
-    def __getattr__(self, name):
-        return self.__func(name, AttributeError)
-
-    def __getitem__(self, name):
-        if name == _SELF:
-            return self
-        return self.__func(name, KeyError)
-
-
-class _Evaluator(PKDict):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # By not passing __builtins__=PKDict(), builtins are implicitly added.
-        # Builtins are useful
-        self.global_ns = PKDict()
-        self.local_ns = _Namespace(self)
-        self.pksetdefault(
-            parent=None,
-            expr_op=lambda: self._expr_op,
-            parser=lambda: self.parent.parser,
+    def push(self, key):
+        f  = inspect.currentframe().f_back.f_back.f_back
+        self.stack.append(
+            PKDict(key=key, func=f.f_code.co_name, line=f.f_lineno),
         )
 
-    def do(self, source, base):
-        try:
-            return self._do(source.content, base)
-        except Exception:
-            pkdlog('Error expanding macros in {}', source)
-            raise
+    def pop(self):
+        self.stack.pop()
 
-    def _dict(self, new, base, xpath):
-        if not isinstance(base, PKDict):
-            if base is not None:
-                raise ValueError(f'mismatched types new={new} base={base}')
-            base = PKDict()
-        for k, v in new.items():
-#TODO: pass v to expr
-            x = self._expr(k, xpath=f'{xpath}.{k}')
-            if isinstance(x, PKDict):
-                #TODO better error msgs
-                assert v is None, f'key={k} must not have a value'
-                base.pkmerge(x)
-            elif isinstance(x, list):
-                raise ValueError(
-                    f'mismatched types expanding macro={k} to value={x}'
-                    + ' into dict={res}',
-                    )
-            else:
-                assert x is not None, f'expanded macro={k} to None'
-                base[x] = self._do(v, base.get(x), xpath=f'{xpath}.{x}')
-        return base
+    def top(self):
+        if self.stack:
+            return self._str(self.stack[-1])
+        else:
+            'None'
 
-    def _do(self, new, base, xpath=''):
-        if isinstance(new, PKDict):
-            return self._dict(new, base, xpath)
-        if isinstance(new, list):
-            return self._list(new, base, xpath)
-        return self._expr(new, xpath)
-
-    def _expr(self, value, xpath=''):
-        try:
-            if not isinstance(value, str):
-                return value
-            k, r  = self.expr_op(self, value)
-            if k:
-                return pykern.pkcollections.canonicalize(r)
-        except Exception:
-            pkdlog('Error expanding macro in text={}', value)
-            raise
-        return value
-
-    @staticmethod
-    def _expr_op(evaluator, value):
-        m = _MACRO_CALL_RE.search(value)
-        if not m:
-            return False, None
-        return True, eval(
-            f'{m.group(1)}({_SELF},{m.group(2)})',
-            evaluator.global_ns,
-            evaluator.local_ns,
-        )
-
-    def _list(self, new, base, xpath):
-        if not isinstance(base, list):
-            if base is not None:
-                raise ValueError(f'mismatched types new={new} base={base}')
-            base = []
-        res = []
-        for i, e in enumerate(new):
-            # lists don't have base values
-            x = self._do(e, None, xpath=f'{xpath}[{i}]')
-            if isinstance(x, list):
-                res.extend(x)
-            else:
-                res.append(x)
-        return res + base
-
-can xpath context be stored in evaluator using contextlib?
-pass parent so can clear parent or just a return value?
-cascade catching errors and printing each level
-fconf_inner() -- already evaluated? yes by default
-fconf_base() on namespace? how would it work?
-fconf_clear()
-convert rsconf
-class for fconf macros, can that be _Namespace?
+    def _str(self, element):
+        return f'{element.func}:{element.line} {str(element.key):20}\n'
