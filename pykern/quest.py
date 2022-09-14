b503a0df00870ef35e83c5994dbbb970e84eafe3
robnagler <github@q33.us>
2022-09-14 20:51:03 +0000

    ckp

---
+++ pykern/quest.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""API wrapper
+
+:copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdc, pkdlog, pkdp
+
+
+class Args:
+    pass
+
+
+class API:
+    pass
+
+
+class Result:
+    pass
+
+
+class Spec:
+    pass
