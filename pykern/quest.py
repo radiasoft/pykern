0f782f55bb0eee315f9b675f10a3e9bc0567e074
Rob Nagler <github@q33.us>
2022-09-26 10:58:56 -0600

    Fix #205 pykern.quest basic module (#278)

---
+++ pykern/quest.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""API wrapper
+
+:copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdc, pkdlog, pkdp
+
+
+class Args:
+    # qargs
+    pass
+
+
+class API(PKDict):
+    # qcall
+    pass
+
+
+class Result:
+    # qres
+    pass
+
+
+class Spec:
+    # qspec
+    pass
