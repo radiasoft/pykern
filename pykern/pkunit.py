342df2e77b9baddbbc15b5203a2353e77ffaf14b
gurhar1133 <ghns1133@gmail.com>
2022-09-30 17:57:12 +0000

    Fix #220: no files fix

--- pykern/pkunit.py
+++ pykern/pkunit.py
@@ -168,13 +168,11 @@ def case_dirs(group_prefix="", ignore_lines=None):
             file_eq(expect_path=e, actual_path=a, ignore_lines=ignore_lines)
 
     d = work_dir()
-    # n = 0
-    if len(pkio.sorted_glob(d.join(group_prefix + "*.in"))) == 0:
-        pkfail("no work files found")
+    n = 0
     for i in pkio.sorted_glob(data_dir().join(group_prefix + "*.in")):
         w = d.join(i.purebasename)
         shutil.copytree(str(i), str(w))
-        # n += 1
+        n += 1
         with pkio.save_chdir(w):
             yield w
         try:
@@ -192,8 +190,8 @@ def case_dirs(group_prefix="", ignore_lines=None):
             _pkdlog("Exception in case_dir={}\n{}", w, f.read())
         # This avoids confusing "during handling of above exception"
         pkfail("See stack above")
-    # if n == 0:
-    #     pkfail("no files found")
+    if n == 0:
+        pkfail(f"No files found for group_prefix={group_prefix}")
 
 
 def data_dir():
--- tests/pkunit1_test.py
+++ tests/pkunit1_test.py
@@ -4,6 +4,7 @@
 :copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+from pykern import pkunit
 import pytest
 
 
@@ -33,3 +34,11 @@ def test_case_dirs_curly_brackets():
     with pkunit.pkexcept("1c1\n< expected\n---\n> {unexpected}"):
         for d in pkunit.case_dirs(group_prefix="curly_brackets"):
             pass
+
+
+def test_no_files():
+    from pykern import pkunit
+
+    with pkunit.pkexcept("no files found"):
+        for d in pkunit.case_dirs(group_prefix="no_files"):
+            continue
