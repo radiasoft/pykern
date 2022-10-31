037f582462759c12e193357562ec8f29ebcd35f9
rorour <ravenmarieorourke@gmail.com>
2022-10-31 21:52:25 +0000

    fix #305: check for newline at eof

--- pykern/pkcli/ci.py
+++ pykern/pkcli/ci.py
@@ -8,23 +8,54 @@ To execute all checks and tests in a CI script, use::
 :copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from pykern.pkdebug import pkdp, pkdlog
-from pykern import pkunit
-from pykern.pkcli import test
+from pykern import pkcli
+from pykern import pkio
 from pykern import pksetup
+from pykern import pkunit
 import re
 
-
-_FILE_TYPE = re.compile(r".py$")
-_EXCLUDE_FILES = re.compile(
+_CHECK_EOF_NEWLINE_EXCLUDE_FILES = re.compile(
+    r"/ext/|node_modules/|^run/|tests/[\w\d/]+_work/|^venv/"
+)
+_CHECK_EOF_NEWLINE_FILE_EXTS = re.compile(r"\.(html|jinja|js|json|md|py|tsx|yml)$")
+_CHECK_PRINTS_EXCLUDE_FILES = re.compile(
     f".*(?:{pkunit.DATA_DIR_SUFFIX}|{pkunit.WORK_DIR_SUFFIX})/"
     + f"|^\\w+/{pksetup.PACKAGE_DATA}/"
     + r"|pkdebug[^/]*\.py$"
     + r"|(?:^|/)\."
     + r"|^run/"
+    + r"|^venv/"
 )
+_CHECK_PRINTS_FILE_EXTS = re.compile(r".py$")
 _PRINT = re.compile(r"(?:\s|^)(?:pkdp|print)\(")
 _PRINT_OK = re.compile(r"^\s*#\s*(?:pkdp|print)\(")
+
+
+def check_eof_newline():
+    """Recursively check repo for files missing newline at end of file.
+
+    Files matching _CHECK_EOF_NEWLINE_FILE_EXTS and not matching
+    _CHECK_EOF_NEWLINE_EXCLUDE_FILES will be checked.
+    """
+
+    def _error(msg):
+        pkcli.command_error("check_eof_newline: {}", msg)
+
+    res = []
+    p = pkio.py_path()
+    n = 0
+    for f in pkio.walk_tree(p, _CHECK_EOF_NEWLINE_FILE_EXTS):
+        f = p.bestrelpath(f)
+        if re.search(_CHECK_EOF_NEWLINE_EXCLUDE_FILES, f):
+            continue
+        n += 1
+        l = pkio.read_text(f).split("\n")
+        if l[-1] != "":
+            res.append(f"{f}")
+    if n == 0:
+        _error("no files found")
+    if res:
+        _error("\n".join(res))
 
 
 def check_prints():
@@ -35,13 +66,7 @@ def check_prints():
     wiki for an explanation of why this check is necessary.
 
     If you really need a print, use `pykern.pkconst.builtin_print`.
-
-    Args:
-        exclude (regex): path pattern to exclude from check
-    ..
     """
-    from pykern import pkio
-    from pykern import pkcli
 
     def _error(msg):
         pkcli.command_error("check_prints: {}", msg)
@@ -49,9 +74,9 @@ def check_prints():
     res = []
     p = pkio.py_path()
     n = 0
-    for f in pkio.walk_tree(p, _FILE_TYPE):
+    for f in pkio.walk_tree(p, _CHECK_PRINTS_FILE_EXTS):
         f = p.bestrelpath(f)
-        if re.search(_EXCLUDE_FILES, f):
+        if re.search(_CHECK_PRINTS_EXCLUDE_FILES, f):
             continue
         n += 1
         for i, l in enumerate(pkio.read_text(f).split("\n"), start=1):
@@ -73,6 +98,7 @@ def run():
     from pykern.pkcli import fmt, test
     from pykern import pkio
 
+    check_eof_newline()
     check_prints()
     fmt.diff(pkio.py_path())
     test.default_command()
