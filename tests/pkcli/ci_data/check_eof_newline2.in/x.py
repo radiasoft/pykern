a44f8b2fb1e292d1140765110bbb79627445f1f2
rorour <ravenmarieorourke@gmail.com>
2022-11-01 19:52:35 +0000

    add test for check_eof_newline

---
+++ tests/pkcli/ci_data/check_eof_newline2.in/x.py
@@ -0,0 +1,3 @@
+def foo():
+    # has newline at eof
+    pass
--- tests/pkcli/ci_test.py
+++ tests/pkcli/ci_test.py
@@ -4,25 +4,24 @@
 :copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+from pykern import pkunit
+from pykern.pkcli import ci
 from pykern.pkdebug import pkdp
-import re
-import pytest
 
 
-def test_run():
-    from pykern.pkcli import ci
-    from pykern import pkio
-    from pykern import pkunit
-
-    for d in pkunit.case_dirs("run"):
+def test_check_eof_newline():
+    for _ in pkunit.case_dirs("check_eof_newline"):
         with pkunit.ExceptToFile():
-            ci.run()
+            ci.check_eof_newline()
 
 
 def test_check_prints():
-    from pykern.pkcli import ci
-    from pykern import pkunit
-
-    for d in pkunit.case_dirs("check_prints"):
+    for _ in pkunit.case_dirs("check_prints"):
         with pkunit.ExceptToFile():
             ci.check_prints()
+
+
+def test_run():
+    for _ in pkunit.case_dirs("run"):
+        with pkunit.ExceptToFile():
+            ci.run()
