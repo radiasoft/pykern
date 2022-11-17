d6ec9acec8f28d0b505bc334fb80b34756658417
Raven <ravenmarieorourke@gmail.com>
2022-11-17 11:31:32 -0700

    fix #305: check for newline at eof (#317)
    fix #305: check for newline at eof

--- tests/pkcli/ci_test.py
+++ tests/pkcli/ci_test.py
@@ -5,24 +5,12 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from pykern.pkdebug import pkdp
-import re
-import pytest
 
 
-def test_run():
-    from pykern.pkcli import ci
-    from pykern import pkio
+def test_all():
     from pykern import pkunit
-
-    for d in pkunit.case_dirs("run"):
-        with pkunit.ExceptToFile():
-            ci.run()
-
-
-def test_check_prints():
     from pykern.pkcli import ci
-    from pykern import pkunit
 
-    for d in pkunit.case_dirs("check_prints"):
+    for d in pkunit.case_dirs():
         with pkunit.ExceptToFile():
-            ci.check_prints()
+            getattr(ci, d.basename.split("-")[0])()
