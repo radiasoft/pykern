2f624bb3e26a6c2c705cf7b3b0374d9d3c7a4a6f
git-user <git-email>
2023-07-16 16:58:18 +0000

    fmt

--- pykern/pkcli/github.py
+++ pykern/pkcli/github.py
@@ -28,7 +28,9 @@ import time
 _GITHUB_HOST = "github.com"
 _GITHUB_URI = "https://" + _GITHUB_HOST
 _GITHUB_API = "https://api." + _GITHUB_HOST
-_WIKI_ERROR_OK = r"fatal: repository 'https://github.com/radiasoft/.*.wiki.git/' not found"
+_WIKI_ERROR_OK = (
+    r"fatal: repository 'https://github.com/radiasoft/.*.wiki.git/' not found"
+)
 _RE_TYPE = type(re.compile(""))
 _MAX_TRIES = 3
 _TEST_REPOS = ["test-pykern-github", "test-pykern-github-no-wiki"]
