ef8196622b3db0caf11bce1184cb49807519c6d8
Rob Nagler <github@q33.us>
2023-09-12 17:03:14 -0600

    Fix #394 added pkcompat.zip_strict for forward compat with py3.10 (#395)
    - added MIME_TYPE and CONTENT_TYPE to pkjson

--- pykern/pkcompat.py
+++ pykern/pkcompat.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-"""Python 2 and 3 compatibility str routines
+"""Backwards and forward compatible Python utilities
 
-:mod:`six` and :mod:`future.utils` do most things, but there are some missing
-things here
+Functions here will be available forever, even when functions are
+removed from Python.
 
-:copyright: Copyright (c) 2015-2022 RadiaSoft LLC.  All Rights Reserved.
+Some functions are no longer necessary, e.g. `unicode_getcwd`. Others
+are still convenient, e.g. `from_bytes` is useful when you don't know
+whether it is a `str` or not.
+
+:copyright: Copyright (c) 2015-2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
+
 """
 # Limit pykern imports so avoid dependency issues for pkconfig
-from __future__ import absolute_import, division, print_function
 import inspect
+import itertools
 import locale
 import os
 import subprocess
+import sys
 
 
 def from_bytes(value):
@@ -90,6 +96,20 @@ def unicode_unescape(value):
     return value.encode("utf-8").decode("unicode-escape")
 
 
+def zip_strict(*iterables):
+    """`zip` where iterables must be exact.
+
+    ``strict`` option was added in 3.10. This function is always strict.
+    Args:
+        iterables (object): things to be zipped
+    Returns:
+        object: Iterator for zipping
+    """
+    if _version_ok(3, 10):
+        return zip(*iterables, strict=True)
+    return _zip_strict(iterables)
+
+
 def _assert_type(value, typ):
     if not isinstance(value, typ):
         raise TypeError(
@@ -99,3 +119,32 @@ def _assert_type(value, typ):
                 type(value),
             ),
         )
+
+
+def _version_ok(major, minor):
+    return sys.version_info >= (major, minor)
+
+
+def _zip_strict(iterables):
+    """Code from https://stackoverflow.com/a/69485272"""
+
+    def _first_tail():
+        nonlocal first_stopped
+        first_stopped = True
+        return
+        yield
+
+    def _zip_tail():
+        if not first_stopped:
+            raise ValueError("first iterable is longer than rest")
+        for x in itertools.chain.from_iterable(rest):
+            raise ValueError(f"left over data={str(x):.100} in iterable")
+            yield
+
+    if len(iterables) < 2:
+        return zip(*iterables)
+    first_stopped = False
+    iterables = iter(iterables)
+    first = itertools.chain(next(iterables), _first_tail())
+    rest = list(map(iter, iterables))
+    return itertools.chain(zip(first, *rest), _zip_tail())
