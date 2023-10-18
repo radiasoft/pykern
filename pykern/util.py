1b4b9438b0a25fc3879c18dfa38efca16c7a7b8e
git-user <git-email>
2023-10-18 18:13:30 +0000

    Fix #405: parser WIP

--- pykern/util.py
+++ pykern/util.py
@@ -15,7 +15,40 @@ import sys
 _DEFAULT_ROOT = "run"
 
 
+def cfg_db_dir(db_root_env_name):
+    """Generate pkconfig parser function with errors for env var name
+
+    Args:
+        db_root_env_name (str): env var name for run dir root
+
+    Returns:
+        function: parser function for pkconfig.init
+    """
+    _NOT_ABS_ERROR = "{}: " + f"{db_root_env_name} must be absolute"
+    _NO_DIR_ERROR = "{}: " + f"{db_root_env_name} must be a directory and exist"
+
+    def cfg_db_dir_parser(v):
+        """Config value or root package's parent or cwd with `_DEFAULT_ROOT`"""
+        print(_NOT_ABS_ERROR.format(v))
+        if not os.path.isabs(v):
+            pkconfig.raise_error(_NOT_ABS_ERROR.format(v))
+        if not os.path.isdir(v):
+            pkconfig.raise_error(_NO_DIR_ERROR.format(v))
+        return pkio.py_path(v)
+
+    return cfg_db_dir_parser
+
+
+
 def init_db_dir(package_object):
+    """Initialize run dir
+
+    Args:
+        package_object (object): python object who's root will hold the run dir
+
+    Returns:
+        py.path.local: absolute path to run dir
+    """
     r = (
         pkio.py_path(
             sys.modules[pkinspect.root_package(package_object)].__file__,
@@ -23,7 +56,6 @@ def init_db_dir(package_object):
         .dirpath()
         .dirpath()
     )
-    pkdp("r={}", r)
     # Check to see if we are in our dev directory. This is a hack,
     # but should be reliable.
     if not r.join("setup.py").check():
