d2616ae77b936a2c7a10236a158049250d4b4891
git-user <git-email>
2024-01-13 02:28:35 +0000

    #429 - ckp

--- pykern/pksetup.py
+++ pykern/pksetup.py
@@ -240,7 +240,6 @@ class ReadTheDocs(DocDist):
         base = self._distribution_to_dict()
         self._fixup()
         self._write_conf(base)
-        print("SPHINX W PKG {}".format(base))
         self._sphinx_apidoc(base)
 
     # still needed?
