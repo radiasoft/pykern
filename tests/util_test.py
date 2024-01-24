8562ee370a5d3c87adc5c9edbefd5290e4645cba
git-user <git-email>
2024-01-24 19:03:24 +0000

    Fix #424: ckp

--- tests/util_test.py
+++ tests/util_test.py
@@ -12,16 +12,12 @@ def test_is_pure_text():
     from pykern import util
 
     a = "a".encode("utf-8")
-    for i, case in enumerate(
-        [
-            not util.is_pure_text(b"\0"),
-            not util.is_pure_text(a + b"\xc2", 2),
-            not util.is_pure_text(
-                b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf"
-            ),
-            util.is_pure_text(b"This is example text"),
-            util.is_pure_text(a + "¡".encode("utf-8"), 2),
-            util.is_pure_text(a + b"\xf0\x9f\x8c\xae", 2),
-        ]
-    ):
+    for case in [
+        not util.is_pure_text(b"\0"),
+        not util.is_pure_text(a + b"\xc2", 2),
+        not util.is_pure_text(b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf"),
+        util.is_pure_text(b"This is example text"),
+        util.is_pure_text(a + "¡".encode("utf-8"), 2),
+        util.is_pure_text(a + b"\xf0\x9f\x8c\xae", 2),
+    ]:
         pkunit.pkok(case, "is_pure_text test case failed")
