751ddd4779a1ce168983d87ffe570e1481726362
git-user <git-email>
2024-01-24 18:56:31 +0000

    Fix #424: ckp

--- tests/util_test.py
+++ tests/util_test.py
@@ -11,15 +11,17 @@ def test_is_pure_text():
     from pykern import pkunit
     from pykern import util
 
-    cases = [
-        not util.is_pure_text(b"\0"),
-        not util.is_pure_text(("a" * 511).encode("utf-8") + b"\xc2", 512),
-        not util.is_pure_text(
-            b"\xeb\xbf\xe4\xa0\xe9\xcf\xa5\n\xe7\xbf\xde\x83\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf"
-        ),
-        util.is_pure_text(b"This is example text"),
-        util.is_pure_text(("a" * 511).encode("utf-8") + "¡".encode("utf-8"), 512),
-        util.is_pure_text(("a" * 511).encode("utf-8") + b"\xf0\x9f\x8c\xae", 512),
-    ]
-    for i, case in enumerate(cases):
-        pkunit.pkok(case, f"case {i + 1} failed")
+    a = "a".encode("utf-8")
+    for i, case in enumerate(
+        [
+            not util.is_pure_text(b"\0"),
+            not util.is_pure_text(a + b"\xc2", 2),
+            not util.is_pure_text(
+                b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf"
+            ),
+            util.is_pure_text(b"This is example text"),
+            util.is_pure_text(a + "¡".encode("utf-8"), 2),
+            util.is_pure_text(a + b"\xf0\x9f\x8c\xae", 2),
+        ]
+    ):
+        pkunit.pkok(case, "is_pure_text test case failed")
