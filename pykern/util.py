76edd53bbab26221b0c05aab53508739b504d07a
git-user <git-email>
2024-01-25 21:59:56 +0000

    Fix #424: ckp

--- pykern/util.py
+++ pykern/util.py
@@ -14,6 +14,11 @@ import sys
 
 _DEFAULT_ROOT = "run"
 _DEV_ONLY_FILES = ("setup.py", "pyproject.toml")
+_STRANGE_CONTROL_CODES = [
+    bytes((n,))
+    for n in range(0, 32)
+    if bytes((n,)) not in b"\x07\x08\t\n\x0b\x0c\r\x1b"
+]
 
 
 def cfg_absolute_dir(value):
@@ -68,9 +73,7 @@ def dev_run_dir(package_object):
 
 
 def is_pure_text(value, test_size=512):
-    """Guesses if value is pure text by attempting to
-    utf-8 decode it. If decode fails, checks if test_size was
-    at the boundary of a valid truncated character
+    """Guesses if value is pure text using heuristics.
 
     Args:
         value (bytes): bytes data
@@ -80,25 +83,35 @@ def is_pure_text(value, test_size=512):
         bool: True if bytes_data is likely pure text, false if likely binary
     """
 
+    def _is_accepted_control_code_ratio(value):
+        c = 0
+        for i in range(len(value)):
+            if bytes([value[i]]) in _STRANGE_CONTROL_CODES:
+                c += 1
+        return c / len(value) < 1 / 3
+
     def _try(chunk):
         try:
             chunk.decode("utf-8", "strict")
-            return True
+            return b"\x00" not in value
         except UnicodeDecodeError:
             return False
 
-    if b"\x00" in value:
+    def _unicode(value, test_size):
+        if len(value) <= test_size:
+            return _try(value)
+        b = value[:test_size]
+        # 4 is maximum length of a utf8 char so if a char
+        # is truncated by test_size, we need to probe back
+        # a bit to find the end of the char.
+        for _ in range(4):
+            if _try(b):
+                return True
+            if len(b) <= 1:
+                return False
+            b = b[:-1]
         return False
-    if len(value) <= test_size:
-        return _try(value)
-    b = value[:test_size]
-    # 4 is maximum length of a utf8 char so if a char
-    # is truncated by test_size, we need to probe back
-    # a bit to find the end of the char.
-    for _ in range(4):
-        if _try(b):
-            return True
-        if len(b) <= 1:
-            return False
-        b = b[:-1]
+
+    if _unicode(value, test_size):
+        return _is_accepted_control_code_ratio(value)
     return False
--- tests/util_test.py
+++ tests/util_test.py
@@ -15,9 +15,12 @@ def test_is_pure_text():
     for case in [
         not util.is_pure_text(b"\0"),
         not util.is_pure_text(a + b"\xc2", 2),
+        not util.is_pure_text(
+            b"\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
+        ),
         not util.is_pure_text(b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf"),
         util.is_pure_text(b"This is example text"),
         util.is_pure_text(a + "¡".encode("utf-8"), 2),
         util.is_pure_text(a + b"\xf0\x9f\x8c\xae", 2),
     ]:
-        pkunit.pkok(case, "is_pure_text test case failed")
+        pkunit.pkok(case, "is_pure_text test case no failed")
