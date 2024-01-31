1ef3d903810a1daa6eb36ca37680903c4f0384e1
git-user <git-email>
2024-01-31 18:01:39 +0000

    Fix #424: ckp

--- tests/util_test.py
+++ tests/util_test.py
@@ -8,39 +8,23 @@
 def test_is_pure_text():
     from pykern import util
     from pykern import pkunit
-    from pykern.pkcollections import PKDict
+
+    def _false(value, is_truncated=False):
+        pkunit.pkeq(False, util.is_pure_text(value, is_truncated=is_truncated))
+
+    def _true(value, is_truncated=False):
+        pkunit.pkeq(True, util.is_pure_text(value, is_truncated=is_truncated))
 
     a = "a".encode("utf-8")
-    for case in [
-        PKDict(
-            value=b"\0"
-            + "Valid text to see that zero byte causes failure".encode("utf-8"),
-            is_truncated=False,
-            expected_result=False,
-        ),
-        PKDict(value=a + b"\xc2", is_truncated=False, expected_result=False),
-        PKDict(
-            value=b"\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f",
-            is_truncated=False,
-            expected_result=False,
-        ),
-        PKDict(
-            value=b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf",
-            is_truncated=False,
-            expected_result=False,
-        ),
-        PKDict(value=b"", is_truncated=False, expected_result=True),
-        PKDict(value=b"This is example text", is_truncated=False, expected_result=True),
-        PKDict(
-            value=b"\x07\x08\t\n\x0b\x0c\r\x0e\x0f",
-            is_truncated=False,
-            expected_result=True,
-        ),
-        PKDict(value=a + "¡".encode("utf-8"), is_truncated=True, expected_result=True),
-        PKDict(value=a + b"\xf0\x9f\x8c\xae", is_truncated=True, expected_result=True),
-    ]:
-        pkunit.pkok(
-            util.is_pure_text(case.value, is_truncated=case.is_truncated)
-            == case.expected_result,
-            f"is_pure_text failed on value={case.value}",
-        )
+    _false(b"\0 one null causes failure in valid text")
+    _false(a + b"\xc2")
+    _false(bytes(range(1, 0x20)))
+    _false(b"\xd4\x16\xc0\xd6\xec\xbf\x92\xe6\x84T\xc9 \xe9\xbf")
+    _false(a + b"\xc2\xc2\xc2\xc2", is_truncated=True)
+    _false(b"\x01"*33 + b"\x07"*67)
+    _true(b"\x01"*32 + b"\x07"*68)
+    _true(b"")
+    _true(b"This is example text")
+    _true(b"\x07\x08\t\n\x0b\x0c\r\x0e\x0f")
+    _true(a + "¡".encode("utf-8"), is_truncated=True)
+    _true(a + b"\xf0\x9f\x8c\xae", is_truncated=True)
