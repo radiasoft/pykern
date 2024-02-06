60e6344b56a376b781dde108a145c7a5eb3cf5f0
git-user <git-email>
2024-02-06 00:40:21 +0000

    altered message

--- pykern/pkcli/test.py
+++ pykern/pkcli/test.py
@@ -18,7 +18,7 @@ import sys
 
 SUITE_D = "tests"
 _COROUTINE_NEVER_AWAITED = re.compile("RuntimeWarning: coroutine .+ was never awaited")
-_TEST_SKIPPED = re.compile(".+ SKIPPED \(.+\)")
+_TEST_SKIPPED = re.compile(r"^.+\s+SKIPPED\s+\(.+\)$", flags=re.MULTILINE)
 _FAILED_ON_WARNINGS = "\n*** FAILED due to warnings. See warnings summary ***\n"
 _TEST_PY = re.compile(r"_test\.py$")
 
@@ -170,6 +170,9 @@ class _Test:
                     # POSIT: pkunit.restart_or_fail uses this
                     return "restart"
             return _fail(output)
+        
+        def _skipped(ouput):
+            return _TEST_SKIPPED.findall(ouput)
 
         def _fail(output):
             self.failures.append(output)
@@ -188,9 +191,8 @@ class _Test:
                 if _COROUTINE_NEVER_AWAITED.search(o):
                     pkio.write_text(output, o + _FAILED_ON_WARNINGS)
                     return _fail(output)
-                for l in o.split("\n"):
-                    if _TEST_SKIPPED.match(l):
-                        sys.stdout.write(l + "\n")
+                if _TEST_SKIPPED.search(o):
+                    return "\n".join(["pass"] + _skipped(o))
                 return "pass"
             except Exception as e:
                 return _except(e, output, restartable)
