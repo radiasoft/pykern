5bf8af6175933338570e8658d7da08da0a0f8c1b
git-user <git-email>
2024-02-07 21:25:12 +0000

    fmt

--- tests/pkcli/test_data/tests/3_test.py
+++ tests/pkcli/test_data/tests/3_test.py
@@ -6,24 +6,20 @@
 """
 from __future__ import absolute_import, division, print_function
 import pytest
-import time
-
-_NOT_SKIPPED = "test_skip not skipped"
 
 
 @pytest.mark.skip(reason="always skips")
 def test_skip():
     from pykern import pkunit
-    from pykern.pkdebug import pkdlog, pkdp
 
-    pkunit.pkfail(_NOT_SKIPPED)
+    pkunit.pkfail("test_skip not skipped")
 
 
 def test_skip_output():
-    import py.path
     from pykern import pkunit
+    import py.path
     
     p = py.path.local(__file__).dirpath("3_test.log")
     with open(p, 'r') as file:
-        c = file.read()
-        if "FAILED" in c: pkunit.pkfail("test_skip failed")
+        if "FAILED" in file.read():
+            pkunit.pkfail("test_skip failed")
