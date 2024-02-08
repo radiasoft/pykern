ebaba51efe16b3d3fec27b25358f5e51182af1c7
robnagler <github@q33.us>
2024-02-08 03:46:40 +0000

    remove test of test inside test

--- tests/pkcli/test_data/tests/3_test.py
+++ tests/pkcli/test_data/tests/3_test.py
@@ -1,28 +1,17 @@
-# -*- coding: utf-8 -*-
 """test pytest skip
 
 :copyright: Copyright (c) 2024 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
 import pytest
 
 
-#@pytest.mark.skip(reason="always skips")
+@pytest.mark.skip(reason="always skips")
 def test_skip():
     from pykern import pkunit
 
     pkunit.pkfail("test_skip not skipped")
 
 
-def test_skip_output(capsys):
-    from pykern import pkcompat
-    from pykern import pkunit
-    import re
-    import sys
-    
-    #print("boo hoo")
-    o, e = capsys.readouterr()
-    print(f"[][]{o}[][]")
-    if not re.search("asdf", pkcompat.from_bytes(o), flags=re.IGNORECASE + re.DOTALL):
-        pkunit.pkfail("[][]{}[][]", o)
+def test_pass():
+    pass
