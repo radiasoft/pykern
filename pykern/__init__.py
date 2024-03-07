8aa5f502ea548d41b581f8ceb9270698e147e310
robnagler <github@q33.us>
2024-03-07 00:19:46 +0000

    hatch: version is vcs hash

--- pykern/__init__.py
+++ pykern/__init__.py
@@ -1,14 +1,8 @@
-# -*- coding: utf-8 -*-
 """pykern package
 
 :copyright: Copyright (c) 2018 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-import pkg_resources
+import importlib.metadata
 
-try:
-    # We only have a version once the package is installed.
-    __version__ = pkg_resources.get_distribution("pykern").version
-except pkg_resources.DistributionNotFound:
-    pass
+__version__ = importlib.metadata.version("pykern")
