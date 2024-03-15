856c4b98963496ff9c970b45507c895c9bdf6bd5
robnagler <github@q33.us>
2024-03-15 16:49:52 +0000

    leave blank pytest_plugin

---
+++ pykern/pytest_plugin.py
@@ -0,0 +1,7 @@
+"""REMOVED
+
+Left for backward compatibility
+
+:copyright: Copyright (c) 2016-2024 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
