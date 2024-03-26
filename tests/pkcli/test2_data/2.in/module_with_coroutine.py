6712a6fcc383f0dad465aeb926df1c1c874dc325
gurhar1133 <ghns1133@gmail.com>
2024-03-26 16:52:20 +0000

    Fix #444: ckp

--- tests/pkcli/test2_data/2.in/module_with_coroutine.py
+++ tests/pkcli/test2_data/2.in/module_with_coroutine.py
@@ -1,12 +1,12 @@
 import asyncio
 
 
-def y():
+def method_with_coroutine():
     async def coroutine():
         asyncio.sleep(0)
     coroutine()
 
 try:
-    asyncio.run(y())
+    asyncio.run(method_with_coroutine())
 except Exception as e:
     pass
--- tests/pkcli/test2_data/2.in/subprocess_test.py
+++ tests/pkcli/test2_data/2.in/subprocess_test.py
@@ -3,6 +3,6 @@ from pykern import pksubprocess
 
 
 def test_subprocess():
-    pksubprocess.check_call_with_signals(["python", "x.py"], output="o.txt")
+    pksubprocess.check_call_with_signals(["python", "module_with_coroutine.py"], output="o.txt")
     with open("o.txt", 'r') as file:
         sys.stderr.write(file.read())
