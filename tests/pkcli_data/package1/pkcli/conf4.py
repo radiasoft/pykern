80278bacc0f96f420136c9366624e74816c96421
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-08-23 11:45:04 -0400

    Fix #499 pkcli gets commands from class Commands (#500)
    - Instantiate the class first
    - Ignore superclass methods

---
+++ tests/pkcli_data/package1/pkcli/conf4.py
@@ -0,0 +1,22 @@
+last_self = None
+last_cmd = None
+last_arg = None
+
+
+def should_not_find():
+    pass
+
+
+class Commands:
+    def __init__(self):
+        global last_self
+
+        last_self = self
+
+    def default_command(self, arg1):
+        global last_cmd, last_arg
+        last_cmd = self.default_command
+        last_arg = arg1
+
+    def _should_not_find():
+        pass
