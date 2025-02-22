f95440f7d3516b6103460fffb400e3d313e67fa8
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-02-22 13:49:51 -0700

    Fix #565 check_prints sees nested pkdp

--- tests/pkcli/ci_test.py
+++ tests/pkcli/ci_test.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 """test ci
 
 :copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from pykern.pkdebug import pkdp
 
 
 def test_all():
