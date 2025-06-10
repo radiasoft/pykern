f48f4c8cb73fbda4db7578a02249467d2d017217
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-06-10 22:56:09 +0000

    session.tables() added; insert/select_or_insert consistent

--- pykern/sql_db.py
+++ pykern/sql_db.py
@@ -2,7 +2,7 @@
 
 This interface is GOING TO CHANGE.
 
-:copyright: Copyright (c) 2024 The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All Rights Reserved.
+:copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 
@@ -90,13 +90,22 @@ class Meta:
         # TODO(robnagler): need to set connection args, e.g. pooling
         self.uri = uri
         self._engine = sqlalchemy.create_engine(uri, echo=_cfg.debug)
-        self._tables = _TableBuilder(self._engine, schema).tables
+        self._table_wraps = _TableBuilder(self._engine, schema).table_wraps
         self._is_sqlite = uri.startswith("sqlite")
 
     def session(self):
         # is context manager so can be with or not
         # TODO(robnagler) keep track of sessions?
         return _Session(self)
+
+    def table(self, name):
+        return self._table_wrap(name).table
+
+    def tables(self, *names):
+        return (self.table(n) for n in names)
+
+    def _table_wrap(self, name):
+        return self._table_wraps[name.lower()]
 
 
 class _Session:
@@ -118,8 +127,8 @@ class _Session:
     def __exit__(self, exc_type, *args, **kwargs):
         self.commit_or_rollback(commit=exc_type is None)
 
-    def column_map(self, table, key_col, value_col, **where):
-        return PKDict({r[key_col]: r[value_col] for r in self.select(table, **where)})
+    def column_map(self, table, key_col, value_col, where):
+        return PKDict({r[key_col]: r[value_col] for r in self.select(table, where)})
 
     def commit(self):
         self.commit_or_rollback(commit=True)
@@ -150,21 +159,18 @@ class _Session:
 
         Args:
             table (str): name of the table to insert into
-            args (tuple): if supplied, ``args[0]`` is (shallow) copied to a `PKDict`
-            kwargs (dict): additional args are applied to `PKDict`
+            values (dict, list, tuple): passed to PKDict
+            kwargs (dict): columns in a `PKDict` (``args`` must be empty)
         Returns:
             PKDict: inserted values with primary_id (if generated)
         """
-        m = self.meta._tables[table.lower()]
-        v = PKDict(args[0]) if args else PKDict()
-        if kwargs:
-            v.pkupdate(kwargs)
-        v = m.fixup_pre_insert(self, v)
-        return m.fixup_post_insert(
+        w = self.meta._table_wrap(table)
+        v = w.fixup_pre_insert(self, self.__varargs(args, kwargs))
+        return w.fixup_post_insert(
             self,
             v,
             getattr(
-                self.execute(m.table.insert().values(v)),
+                self.execute(w.table.insert().values(v)),
                 "inserted_primary_key",
                 None,
             ),
@@ -173,49 +179,56 @@ class _Session:
     def rollback(self):
         self.commit_or_rollback(commit=False)
 
-    def select(self, table_or_stmt, **where):
+    def select(self, table_or_stmt, where=None):
         def _stmt(table):
             rv = sqlalchemy.select(table)
-            if where:
-                rv.where(
-                    *(
-                        sqlalchemy.sql.operators.eq(table.columns[k], v)
-                        for k, v in where.items()
-                    )
+            if not where:
+                return rv
+            return rv.where(
+                *(
+                    sqlalchemy.sql.operators.eq(table.columns[k], v)
+                    for k, v in where.items()
                 )
-            return rv.limit(500)
+            )
 
         return self.execute(
-            _stmt(self.meta._tables[table_or_stmt.lower()].table)
+            _stmt(self.table(table_or_stmt))
             if isinstance(table_or_stmt, str)
             else table_or_stmt
         )
 
     def select_max_primary_id(self, table):
-        m = self.meta._tables[table.lower()]
+        w = self.meta._table_wrap(table)
         return self.execute(
             sqlalchemy.select(
-                sqlalchemy.func.max(m.table.columns[m.primary_id]),
+                sqlalchemy.func.max(w.table.columns[w.primary_id]),
             )
         ).scalar()
 
-    def select_or_insert(self, table, **values):
-        if m := self.select_one_or_none(table, **values):
+    def select_or_insert(self, table, *args, **kwargs):
+        v = self.__varargs(args, kwargs)
+        if m := self.select_one_or_none(table, where=v):
             return m
-        return self.insert(table, **values)
+        return self.insert(table, v)
 
-    def select_one(self, table_or_stmt, **where):
-        if rv := self.select_one_or_none(table_or_stmt, **where):
+    def select_one(self, table_or_stmt, where=None):
+        if rv := self.select_one_or_none(table_or_stmt, where):
             return rv
         raise NoRows(table_or_stmt=table_or_stmt, where=where)
 
-    def select_one_or_none(self, table_or_stmt, **where):
+    def select_one_or_none(self, table_or_stmt, where):
         rv = None
-        for x in self.select(table_or_stmt, **where):
+        for x in self.select(table_or_stmt, where):
             if rv is not None:
                 raise MoreThanOneRow(table_or_stmt=table_or_stmt, where=where)
             rv = x
         return rv
+
+    def table(self, name):
+        return self.meta.table(name)
+
+    def tables(self, *names):
+        return self.meta.tables(*names)
 
     def __conn(self):
         if self._conn is None:
@@ -223,8 +236,19 @@ class _Session:
             self._txn = self._conn.begin()
         return self._conn
 
+    def __varargs(self, args, kwargs):
+        if len(args) == 1:
+            if kwargs:
+                raise AssertionError(
+                    f"may only have one args={args} or kwargs={kwargs}"
+                )
+            return PKDict(args[0])
+        if len(args) > 1:
+            raise AssertionError(f"too many args={args}, must be one or zero)")
+        return PKDict(kwargs)
 
-class _Table:
+
+class _TableWrap:
     """Wraps an `sqlalchemy.Table`
 
     See `sqlalchemy.db` for how to get instances of this class.
@@ -304,10 +328,10 @@ class _TableBuilder:
 
     def __init__(self, engine, schema):
         self.metadata = sqlalchemy.MetaData()
-        self.tables = PKDict()
+        self.table_wraps = PKDict()
         self._primary_id_cols = PKDict()
         for s in self._parser(schema):
-            self.tables[s.name] = self._table_create(s)
+            self.table_wraps[s.name] = self._table_create(s)
         self.metadata.create_all(bind=engine)
 
     def _parser(self, schema):
@@ -427,7 +451,9 @@ class _TableBuilder:
                     f"invalid primary_id or not found foreign id col_spec={col_spec}"
                 )
 
-        return _Table(sqlalchemy.Table(*_args()))
+        return _TableWrap(sqlalchemy.Table(*_args()))
+
+
 # At end to avoid clutter at top
 _RESERVED_IN_SQLITE_POSTGRES = frozenset(
     {
--- tests/sql_db_test.py
+++ tests/sql_db_test.py
@@ -8,42 +8,82 @@ _PATH = "test.sqlite3"
 
 
 def test_1():
+    # Must be first
     from pykern.pkcollections import PKDict
-    from pykern import pkunit, sql_db, pkdebug
+    from pykern import pkunit, pkdebug
 
     with pkunit.save_chdir_work() as d:
-        m = sql_db.Meta(
-            uri=_uri(d),
-            schema=PKDict(
-                t1=PKDict(
-                    t1_id="primary_id 1",
-                    a_name="str 64",
-                    a_text="str 1024",
-                    created="date_time index",
-                    unique=(("a_name", "a_text"),),
-                ),
-                t2=PKDict(
-                    t2_id="primary_id 2",
-                    t1_id="primary_id",
-                    a_int="int 32",
-                    a_float="float 32",
-                    a_double="float 64 nullable",
-                    a_bigint="int 64 unique",
-                    index=(("t1_id", "t2_id"),),
-                ),
+        m = _meta(d)
+        _validate_schema()
+        _inserts(m)
+        _selects(m)
+
+
+def _inserts(meta):
+    with meta.session() as s:
+        r = s.insert("t1", a_name="William", a_int=1, a_text="war and peace")
+        s.insert(
+            "T2",
+            t1_id=r.t1_id,
+            a_float=3,
+            a_double=1e1000,
+            a_bigint=int(1e11),
+        )
+        r = s.insert(
+            "T1", dict(a_name="Mildred", a_int=333, a_text="hitch hiker's guide")
+        )
+        s.insert("t2", dict(t1_id=r.t1_id, a_float=2, a_bigint=222))
+        s.insert("t2", dict(t1_id=r.t1_id, a_float=3, a_bigint=333))
+
+
+def _meta(dir_path):
+    from pykern.pkcollections import PKDict
+    from pykern import sql_db
+
+    return sql_db.Meta(
+        uri=_uri(dir_path),
+        schema=PKDict(
+            T1=PKDict(
+                t1_id="primary_id 1",
+                a_name="str 64",
+                a_text="text",
+                a_int="int 32",
+                created="date_time index",
+                unique=(("a_name", "a_text"),),
+            ),
+            t2=PKDict(
+                t2_id="primary_id 2",
+                t1_id="primary_id",
+                a_float="float 32",
+                a_double="float 64 nullable",
+                a_bigint="int 64 unique",
+                index=(("t1_id", "t2_id"),),
+            ),
+        ),
+    )
+
+
+def _selects(meta):
+    from pykern import pkunit, pkdebug
+    import sqlalchemy
+
+    with meta.session() as s:
+        r = s.select_one("t1", where=dict(a_name="Mildred"))
+        pkunit.pkeq("hitch hiker's guide", r.a_text)
+        t1, t2 = s.tables("t1", "t2")
+        r = s.execute(
+            sqlalchemy.select(
+                t1,
+                t2,
+            )
+            .join(
+                t1,
+                t2.c.a_bigint == t1.c.a_int,
+            )
+            .where(
+                t1.c.a_int == 333,
             ),
         )
-        _validate_schema()
-        with m.session() as s:
-            r1 = s.insert("t1", PKDict(a_name="William", a_text="war and peace"))
-            r2 = s.insert(
-                "t2",
-                t1_id=r1.t1_id,
-                a_int=1,
-                a_float=3,
-                a_double=1e1000,
-                a_bigint=int(1e11),
-            )
 
 
 def _uri(d):
@@ -59,6 +99,7 @@ def _validate_schema():
     i = []
     for l in re.split(
         "\s*\n\s*",
+        # Assumes in same directory
         subprocess.check_output(["sqlite3", _PATH, ".schema"], text=True),
     ):
         if len(l) <= 0:
