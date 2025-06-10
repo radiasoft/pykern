183c17404e374d5f25f6c9bf432d0d6793738ffa
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-06-10 17:57:15 +0000

    may be good to go

--- pykern/sql_db.py
+++ pykern/sql_db.py
@@ -15,53 +15,200 @@ import sqlalchemy
 import sqlalchemy.sql.operators
 import sys
 
-_RESERVED_IN_SQLITE_POSTGRES = frozenset({
-    "abort", "action", "add", "after", "all", "alter", "analyze", "analyse", "and", "any",
-    "array", "as", "asc", "asymmetric", "attach", "authorization", "autoincrement", "before",
-    "begin", "between", "binary", "both", "by", "cascade", "case", "cast", "check", "collate",
-    "column", "commit", "concurrently", "conflict", "constraint", "cross", "create", "current_catalog",
-    "current_date", "current_role", "current_time", "current_timestamp", "current_user", "database",
-    "default", "deferrable", "deferred", "delete", "desc", "detach", "distinct", "do", "drop", "each",
-    "else", "end", "escape", "except", "exclusive", "exists", "explain", "false", "fetch", "for",
-    "foreign", "from", "full", "glob", "grant", "group", "having", "if", "ignore", "immediate", "in",
-    "index", "initially", "inner", "insert", "instead", "intersect", "into", "is", "isnull", "join",
-    "key", "leading", "left", "like", "limit", "localtime", "localtimestamp", "match", "natural", "no",
-    "not", "notnull", "null", "of", "offset", "on", "only", "or", "order", "outer", "overlaps", "plan",
-    "placing", "pragma", "primary", "query", "raise", "recursive", "references", "regexp", "reindex",
-    "release", "rename", "replace", "restrict", "right", "rollback", "row", "savepoint", "select",
-    "session_user", "set", "similar", "some", "symmetric", "table", "temp", "temporary", "then", "to",
-    "trailing", "transaction", "trigger", "true", "union", "unique", "update", "using", "user",
-    "values", "variadic", "vacuum", "verbose", "view", "virtual", "when", "where", "window", "with",
-    "without"
-})
+_NULLABLE_DECL = "nullable"
+
+_RESERVED_IN_SQLITE_POSTGRES = frozenset(
+    {
+        # sqlalchemy word we use so can't be column name
+        _NULLABLE_DECL,
+        "abort",
+        "action",
+        "add",
+        "after",
+        "all",
+        "alter",
+        "analyze",
+        "analyse",
+        "and",
+        "any",
+        "array",
+        "as",
+        "asc",
+        "asymmetric",
+        "attach",
+        "authorization",
+        "autoincrement",
+        "before",
+        "begin",
+        "between",
+        "binary",
+        "both",
+        "by",
+        "cascade",
+        "case",
+        "cast",
+        "check",
+        "collate",
+        "column",
+        "commit",
+        "concurrently",
+        "conflict",
+        "constraint",
+        "cross",
+        "create",
+        "current_catalog",
+        "current_date",
+        "current_role",
+        "current_time",
+        "current_timestamp",
+        "current_user",
+        "database",
+        "default",
+        "deferrable",
+        "deferred",
+        "delete",
+        "desc",
+        "detach",
+        "distinct",
+        "do",
+        "drop",
+        "each",
+        "else",
+        "end",
+        "escape",
+        "except",
+        "exclusive",
+        "exists",
+        "explain",
+        "false",
+        "fetch",
+        "for",
+        "foreign",
+        "from",
+        "full",
+        "glob",
+        "grant",
+        "group",
+        "having",
+        "if",
+        "ignore",
+        "immediate",
+        "in",
+        "index",
+        "initially",
+        "inner",
+        "insert",
+        "instead",
+        "intersect",
+        "into",
+        "is",
+        "isnull",
+        "join",
+        "key",
+        "leading",
+        "left",
+        "like",
+        "limit",
+        "localtime",
+        "localtimestamp",
+        "match",
+        "natural",
+        "no",
+        "not",
+        "notnull",
+        "null",
+        "of",
+        "offset",
+        "on",
+        "only",
+        "or",
+        "order",
+        "outer",
+        "overlaps",
+        "plan",
+        "placing",
+        "pragma",
+        "primary",
+        "query",
+        "raise",
+        "recursive",
+        "references",
+        "regexp",
+        "reindex",
+        "release",
+        "rename",
+        "replace",
+        "restrict",
+        "right",
+        "rollback",
+        "row",
+        "savepoint",
+        "select",
+        "session_user",
+        "set",
+        "similar",
+        "some",
+        "symmetric",
+        "table",
+        "temp",
+        "temporary",
+        "then",
+        "to",
+        "trailing",
+        "transaction",
+        "trigger",
+        "true",
+        "union",
+        "unique",
+        "update",
+        "using",
+        "user",
+        "values",
+        "variadic",
+        "vacuum",
+        "verbose",
+        "view",
+        "virtual",
+        "when",
+        "where",
+        "window",
+        "with",
+        "without",
+    }
+)
 
 _PRIMARY_ID_DECL = "primary_id"
 
-_STYPE_MAP = PKDict({
-    "bool": sqlalchemy.types.Boolean(),
-    "date_time": sqlalchemy.types.DateTime(),
-    "float 32": sqlalchemy.types.Float(precision=24),
-    "float 64": sqlalchemy.types.Float(precision=53),
-    "int 32": sqlalchemy.types.Integer(),
-    "int 64": sqlalchemy.types.BigInteger(),
-    # str & primary_id are special cases
-    "text": sqlalchemy.types.Text(),
-})
+_COL_MODIFIERS = ("index", _NULLABLE_DECL, "primary_key", "unique")
+
+_STYPE_MAP = PKDict(
+    {
+        "bool": sqlalchemy.types.Boolean(),
+        "date_time": sqlalchemy.types.DateTime(),
+        "float 32": sqlalchemy.types.Float(precision=24),
+        "float 64": sqlalchemy.types.Float(precision=53),
+        "int 32": sqlalchemy.types.Integer(),
+        "int 64": sqlalchemy.types.BigInteger(),
+        # str & primary_id are special cases
+        "text": sqlalchemy.types.Text(),
+    }
+)
 
 _PRIMARY_ID_INC = 1000
 
 _PRIMARY_ID_MOD = _PRIMARY_ID_INC // 10
 
-o_PRIMARY_ID_STYPE = sqlalchemy.types.BigInteger()
+_PRIMARY_ID_STYPE = sqlalchemy.types.BigInteger()
 
 _QUALIFIER = re.compile(r"^\d+$")
 
 _STR_DECL = "str"
 
 # Denormalization is ok since _STYPE_MAP is checked.
-_TYPE_NAMES = frozenset({
-    "bool", "date_time", "float", "int", _PRIMARY_ID_DECL, _STR_DECL, "text"
-})
+_TYPE_NAMES = frozenset(
+    {"bool", "date_time", "float", "int", _PRIMARY_ID_DECL, _STR_DECL, "text"}
+)
+
 
 class BaseExc(Exception):
     """Superclass for sll exceptions in this module"""
@@ -91,13 +238,11 @@ class MoreThanOneRow(BaseExc):
 
 
 class Meta:
-    def __init__(self, uri, decls):
-        @pykern.pkconfig.parse_none
+    def __init__(self, uri, schema):
         # TODO(robnagler): need to set connection args, e.g. pooling
         self.uri = uri
         self._engine = sqlalchemy.create_engine(uri, echo=_cfg.debug)
-        self._table_builder = _TableBuilder(self._engine, decls)
-        self._tables = _table_builder.tables
+        self._tables = _TableBuilder(self._engine, schema).tables
         self._is_sqlite = uri.startswith("sqlite")
 
     def session(self):
@@ -113,11 +258,11 @@ class _Session:
     on success or rollback on an exception.
     """
 
-    def __init__(self, schema, *args, **kwargs):
+    def __init__(self, meta, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._conn = None
         self._txn = None
-        self.schema = schema
+        self.meta = meta
 
     def __enter__(self):
         return self
@@ -162,7 +307,7 @@ class _Session:
         Returns:
             PKDict: inserted values with primary_id (if generated)
         """
-        m = self.schema._tables[lc(table)]
+        m = self.meta._tables[lc(table)]
         v = PKDict(args[0]) if args else PKDict()
         if kwargs:
             v.pkupdate(kwargs)
@@ -193,13 +338,13 @@ class _Session:
             return rv.limit(500)
 
         return self.execute(
-            _stmt(self.schema._tables[lc(table_or_stmt)].table)
+            _stmt(self.meta._tables[lc(table_or_stmt)].table)
             if isinstance(table_or_stmt, str)
             else table_or_stmt
         )
 
     def select_max_primary_id(self, table):
-        m = self.schema._tables[lc(table)]
+        m = self.meta._tables[lc(table)]
         return self.execute(
             sqlalchemy.select(
                 sqlalchemy.func.max(m.table.columns[m.primary_id]),
@@ -226,125 +371,9 @@ class _Session:
 
     def __conn(self):
         if self._conn is None:
-            self._conn = self.schema._engine.connect()
+            self._conn = self.meta._engine.connect()
             self._txn = self._conn.begin()
         return self._conn
-
-
-class _TableBuilder:
-    """Defines the schema for SQLAlchemy"""
-
-    def __init__(self, engine, decls):
-        self.metadata = sqlalchemy.MetaData()
-        self.tables = PKDict()
-        self._primary_id_cols = PKDict()
-        self._parser(decls)
-        self.metadata.create_all(bind=engine)
-
-    def _parser(self, decls):
-
-        def _col_decl(name, decl):
-            rv = PKDict(kwargs=PKDict(), name=name)
-            for d in lc(decl).split(" "):
-                if d in ("index", "unique", "nullable"):
-                    rv.kwargs[d] = True
-                elif _QUALIFIER.search(d):
-                    if qualifier in rv:
-                        raise AssertionError(f"duplicate qualifier={d} field={name}")
-                    rv.stype_qualifier = d
-                elif d in _TYPE_NAMES:
-                    rv.stype_name = d
-                else:
-                    raise AssertionError(f"invalid declaration={d} field={name}")
-            # put in right form here [name, stype, kwargs)
-            rv.stype = _stype(rv, decl)
-            return rv
-
-        def _ident(name, name_set, which):
-            rv = lc(name)
-            if rv in _RESERVED_IN_SQLITE_POSTGRES:
-                raise AssertionError(f"{which}={rv} is a reserved word")
-            if rv in name_set:
-                raise AssertionError(f"{which}={rv} is a duplicate (case insensitive)")
-            name_set.add(rv)
-            return rv
-
-
-        def _stype(spec, decl):
-            if not (t := spec.pkdel("stype_name")):
-                raise AssertionError(f"missing type name col={spec.name} decl={decl}")
-            if q := spec.pkdel("stype_qualifier"):
-                if t == _STR_DECL:
-                    return sqlalchemy.types.String(q)
-                if t == _PRIMARY_ID_DECL:
-                    if not (0 < q < _PRIMARY_ID_MOD):
-                        raise AssertionError(f"{_PRIMARY_ID_DECL} qualifier must be between 0 and {_PRIMARY_ID_MOD}")
-                    # SIDE-EFFECT: nested function so limited scope
-                    spec.sequence = q
-                    return _PRIMARY_ID_STYPE
-                if t in _STYPE_MAP:
-                    raise AssertionError(f"type={t} does not accept a qualifier={q}")
-                t += f" {q}"
-            if not (rv := _STYPE_MAP.get(t)):
-                raise AssertionError(f"type={t} not found or limited qualifier choices")
-            return rv
-
-        def _table_decl(table_name, tables, decl):
-            rv = PKDict(name=_ident(table_name, tables, "table"), cols=[])
-            u = PKDict()
-            for k, v in decl.items():
-                if k in ("index", "unique"):
-                    rv[k] = v
-                else:
-                    rv.cols.append(_col_decl(_ident(k, u, "column"), v))
-            rv.pksetdefault(index=(), unique=())
-            return rv
-
-        return self.tables[n] = _Table(n, _table(n, *args, **kwargs))
-
-    def _table(self, table_name, *cols, index=(), unique=()):
-        def _args():
-            rv = [table_name, self.metadata]
-            for x in cols:
-                rv.append(_col_arg(x))
-            for x in index:
-                rv.append(sqlalchemy.Index(*x))
-            for x in unique:
-                rv.append(sqlalchemy.UniqueConstraint(*x))
-            return rv
-
-        def _col_arg(decl):
-            a = [decl.pkdel("name"), decl.pkdel("stype")]
-            if decl.stype is _PRIMARY_ID_STYPE:
-                _col_primary_id(decl, a)
-            if decl.get("unique"):
-                decl.index = True
-            decl.pksetdefault(nullable=False)
-            return sqlalchemy.Column(*a, **decl)
-
-        def _col_primary_id(decl, args):
-            n = args[0]
-            if s := decl.pkdel("sequence"):
-                args.append(
-                    sqlalchemy.Sequence(
-                        f"{n}_seq",
-                        start=PRIMARY_ID_INC + s,
-                        increment=PRIMARY_ID_INC,
-                    )
-                )
-                if n in self._primary_id_cols:
-                    raise AssertionError(
-                        f"duplicate sequence for column={n} table={table_name}"
-                    )
-                self._primary_id_cols[n] = table_name
-                decl.primary_key = True
-            elif i := self._primary_id_cols.get(n):
-                args.append(sqlalchemy.ForeignKey(f"{i}.{n}"))
-                decl.index = True
-            else:
-                raise AssertionError(f"invalid use of primary_id name={n} decl={decl}")
-
-        return sqlalchemy.Table(*_args())
 
 
 class _Table:
@@ -411,9 +440,135 @@ class _Table:
         """
         if self.has_created and "created" not in values:
             values.created = datetime.datetime.utcnow()
-        if session.schema._is_sqlite and self.has_primary_id and self.primary_id not in values:
+        if (
+            session.meta._is_sqlite
+            and self.has_primary_id
+            and self.primary_id not in values
+        ):
             v = session.select_max_primary_id(self.name)
             values[self.primary_id] = (
                 self.primary_id_start if v is None else v + PRIMARY_ID_INC
             )
         return values
+
+
+class _TableBuilder:
+    """Defines the schema for SQLAlchemy"""
+
+    def __init__(self, engine, schema):
+        self.metadata = sqlalchemy.MetaData()
+        self.tables = PKDict()
+        self._primary_id_cols = PKDict()
+        for s in self._parser(schema):
+            self.tables[s.name] = self._table_create(s)
+        self.metadata.create_all(bind=engine)
+
+    def _parser(self, schema):
+
+        def _col_decl(name, decl):
+            rv = PKDict(kwargs=PKDict(), name=name)
+            for d in lc(decl).split(" "):
+                if d in _COL_MODIFIERS:
+                    rv.kwargs[d] = True
+                elif _QUALIFIER.search(d):
+                    if qualifier in rv:
+                        raise AssertionError(f"duplicate qualifier={d} field={name}")
+                    rv.stype_qualifier = int(d)
+                elif d in _TYPE_NAMES:
+                    rv.stype_name = d
+                else:
+                    raise AssertionError(f"invalid declaration={d} field={name}")
+            rv.stype = _stype(rv, decl)
+            return rv
+
+        def _ident(name, which, name_set):
+            rv = lc(name)
+            if rv in _RESERVED_IN_SQLITE_POSTGRES:
+                raise AssertionError(f"{which}={rv} is a reserved word")
+            if rv in name_set:
+                raise AssertionError(f"{which}={rv} is a duplicate (case insensitive)")
+            name_set.add(rv)
+            return rv
+
+        def _stype(spec, decl):
+            if (t := spec.pkdel("stype_name")) is None:
+                raise AssertionError(f"missing type name col={spec.name} decl={decl}")
+            if (q := spec.pkdel("stype_qualifier")) is not None:
+                if t == _STR_DECL:
+                    return sqlalchemy.types.String(q)
+                if t == _PRIMARY_ID_DECL:
+                    if not (0 < q < _PRIMARY_ID_MOD):
+                        raise AssertionError(
+                            f"{_PRIMARY_ID_DECL} qualifier must be between 0 and {_PRIMARY_ID_MOD}",
+                        )
+                    # SIDE-EFFECT: nested function so limited scope
+                    spec.sequence = q
+                    return _PRIMARY_ID_STYPE
+                if t in _STYPE_MAP:
+                    raise AssertionError(f"type={t} does not accept a qualifier={q}")
+                t += f" {q}"
+            if (rv := _STYPE_MAP.get(t)) is None:
+                raise AssertionError(f"type={t} not found or limited qualifier choices")
+            return rv
+
+        def _table_decl(table_name, decl, table_set):
+            rv = PKDict(name=_ident(table_name, "table", table_set), cols=[])
+            s = set()
+            for k, v in decl.items():
+                if k in ("index", "unique"):
+                    rv[k] = v
+                else:
+                    rv.cols.append(_col_decl(_ident(k, "column", s), v))
+            rv.pksetdefault(index=(), unique=())
+            return rv
+
+        s = set()
+        # parse fully before creating, don't return a generator
+        return [_table_decl(k, v, s) for k, v in schema.items()]
+
+    def _table_create(self, spec):
+        def _args():
+            rv = [spec.name, self.metadata]
+            for x in spec.cols:
+                rv.append(_col(x))
+            for x in spec.get("index", ()):
+                rv.append(sqlalchemy.Index(*x))
+            for x in spec.get("unique", ()):
+                rv.append(sqlalchemy.UniqueConstraint(*x))
+            return rv
+
+        def _col(col_spec):
+            a = [col_spec.pkdel("name"), col_spec.pkdel("stype")]
+            if a[1] is PRIMARY_ID_STYPE:
+                _col_primary_id(col_spec, a)
+            if col_spec.get("unique"):
+                col_spec.index = True
+            col_spec.pksetdefault(nullable=False)
+            return sqlalchemy.Column(*a, **col_spec)
+
+        def _col_primary_id(col_spec, args):
+            n = args[0]
+            if s := col_spec.pkdel("sequence"):
+                if n in self._primary_id_cols:
+                    raise AssertionError(
+                        f"duplicate primary_id decl column={n} table={spec.name}"
+                    )
+                self._primary_id_cols[n] = spec.name
+                args.append(
+                    sqlalchemy.Sequence(
+                        f"{n}_seq",
+                        start=PRIMARY_ID_INC + s,
+                        increment=PRIMARY_ID_INC,
+                    )
+                )
+                col_spec.primary_key = True
+            elif i := self._primary_id_cols.get(n):
+                args.append(sqlalchemy.ForeignKey(f"{i}.{n}"))
+                col_spec.index = True
+            else:
+                # TODO more flexible some day
+                raise AssertionError(
+                    f"invalid primary_id or not found foreign id col_spec={col_spec}"
+                )
+
+        return sqlalchemy.Table(*_args())
