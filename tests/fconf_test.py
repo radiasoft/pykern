1fe9df5e2c04a9236cae4de3ef71fb257ce08ae3
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-07-05 00:53:47 +0000

    Fix #580 parse_all incorrect when yaml - added a test

--- tests/fconf_test.py
+++ tests/fconf_test.py
@@ -4,8 +4,6 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 
-import pytest
-
 
 def test_parser():
     from pykern import pkunit, fconf, pkio, pkjson
@@ -22,3 +20,18 @@ def test_parser():
         r = p.result
         r.macros = [f'{p.name}({",".join(p.params)})' for p in p.macros.values()]
         pkjson.dump_pretty(r, filename="res.json")
+
+
+def test_parse_all():
+    from pykern import pkunit, fconf, pkjson
+
+    d = pkunit.work_dir().join("parseall").ensure(dir=1)
+    d.join("1.yml").write("---\npi: 3.14\ne: 2.7\n")
+    d.join("2.yaml").write("---\npi: ${e}\n")
+    d.join("3.yml").write("---\ne: 1.6\n")
+    d.join("not-seen.yml").write("---\nnot-seen: false\n")
+    a = fconf.parse_all(d, glob="?")
+    pkjson.dump_pretty(a, filename=d.join("res.json"))
+    pkunit.pkok("not-seen" not in a, "not-seen got parsed values={}", a)
+    pkunit.pkeq(2.7, a.pi)
+    pkunit.pkeq(1.6, a.e)
