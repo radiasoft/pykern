eba3e3d18d9527d4f4fba5da559c18304606cd5e
Paul Moeller <moeller@radiasoft.net>
2025-09-16 23:03:30 +0000

    fixed wrong dup removal

--- tests/pkinspect_test.py
+++ tests/pkinspect_test.py
@@ -103,7 +103,7 @@ def test_import_submodule():
 
         _dev("error_in_p2", "err2", None, None)
         _dev("error_in_p1", "err2", None, ("p1", "p2"))
-        _dev("error_in_p1", "err2", None, ("p1", "p2"))
+        _dev("error_in_p2", "err4", None, ("p1", "p2"))
         _dev("find module=subpkg1.mod737", "mod737", None, ("p1", "p2"))
         _dev("find module=subpkg737.mod1", "mod1", "subpkg737", ("p1", "p2"))
         _dev(r"root_packages=\('not_root_pkg',\)", "mod1", "subpkg1", ("not_root_pkg",))
