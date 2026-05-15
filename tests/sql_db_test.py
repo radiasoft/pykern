0e89be2ebb7c031bf34fc6827927b8dd1e25a6c2
EY <98988862+eloiseyang@users.noreply.github.com>
2026-05-15 11:21:45 -0700

    Check that we get the expected keys.

--- tests/sql_db_test.py
+++ tests/sql_db_test.py
@@ -97,9 +97,10 @@ def _selects(meta):
 
     with meta.session() as s:
         r = s.select_one("t1", where=dict(a_name="Mildred"))
-        PKDict(r)
         pkunit.pkeq("hitch hiker's guide", r.a_text)
         t1, t2 = s.t.t1, s.t.t2
+        d = PKDict(r)
+        pkunit.pkeq(t1.c.keys(), list(d.keys()))
         r = s.execute(
             sqlalchemy.select(
                 t1,
